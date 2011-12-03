aa4d5472c4dccb77128471926b4ecc62e480cba4
Charles Oliver Nutter <headius@headius.com>
2011-12-03 11:54:03 -0600

    Modifications to support building on Windows. Fixes #8.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,10 @@
 require 'mkmf'
 extension_name = 'atomic_reference'
 dir_config(extension_name)
+
+case CONFIG["arch"]
+when /mswin32|mingw/
+    $CFLAGS += " -march=i686"
+end
+
 create_makefile(extension_name)
