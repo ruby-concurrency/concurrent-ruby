c291231e9c8e34275eebc228891bf7a4bc4608f2
Charles Oliver Nutter <headius@headius.com>
2013-04-16 09:16:56 -0500

    Allow forcing fallback impl.

--- lib/atomic.rb
+++ lib/atomic.rb
@@ -11,8 +11,15 @@
 # limitations under the License.
 
 begin
-  ruby_engine = defined?(RUBY_ENGINE)? RUBY_ENGINE : 'ruby'
+  # force fallback impl with FORCE_ATOMIC_FALLBACK=1
+  if /[^0fF]/ =~ ENV['FORCE_ATOMIC_FALLBACK']
+    ruby_engine = 'fallback'
+  else
+    ruby_engine = defined?(RUBY_ENGINE)? RUBY_ENGINE : 'ruby'
+  end
+  
   require "atomic/#{ruby_engine}"
 rescue LoadError
+  warn "#{__FILE__}:#{__LINE__}: unsupported Ruby engine `#{RUBY_ENGINE}', using less-efficient Atomic impl"
   require 'atomic/fallback'
 end
