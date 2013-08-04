72fc69321c02baaa3565c791fdde478fb3829406
Jerry D'Antonio <stumpjumper@gmail.com>
2013-08-04 19:07:30 -0400

    Ran stress tests of TcpDemultiplexer

--- reactor_test.rb
+++ reactor_test.rb
@@ -3,6 +3,8 @@ require 'rubygems'
 require 'concurrent'
 
 require 'drb/drb'
+require 'socket'
+
 require 'faker'
 require 'functional'
 
@@ -15,12 +17,58 @@ def with_commas(n)
   n.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse
 end
 
+###############################################################################
+# TcpDemultiplexer
 
+def kill_tcp_echo_server
+  there = TCPSocket.open(TCP_HOST, TCP_PORT)
+  there.puts(Concurrent::TcpDemultiplexer.format_message(:kill))
+  there.close
+  there = nil
+end
 
+def tcp_echo_test(count, verbose = true)
+  there = TCPSocket.open(TCP_HOST, TCP_PORT)
+
+  good = 0
+
+  duration = timer do
+    count.times do |i|
+      message = Faker::Company.bs
+      puts "Sending  '#{message}'" if verbose
+      there.puts(Concurrent::TcpDemultiplexer.format_message(:echo, message))
+      result, echo = Concurrent::TcpDemultiplexer.get_message(there)
+      puts "Received '#{echo}'" if verbose
+      good += 1 if echo.first == message
+    end
+  end
+
+  there.close
+  there = nil
+
+  messages_per_second = count / duration
+  success_rate = good / count.to_f * 100.0
+
+  puts "Sent #{count} messages. Received #{good} good responses and #{count - good} bad."
+  puts "The total processing time was %0.3f seconds." % duration
+  puts "That's %i messages per second with a %0.1f success rate, for those keeping score." % [messages_per_second, success_rate]
+  puts "And we're done!"
+end
 
 def tcp_echo_server(verbose = true)
   demux = Concurrent::TcpDemultiplexer.new(host: TCP_HOST, port: TCP_PORT)
   reactor = Concurrent::Reactor.new(demux)
+
+  count = 0
+  reactor.add_handler(:echo) do |*args|
+    puts "Received: '#{args.first}'" if verbose
+    count += 1
+    args.first
+  end
+
+  reactor.add_handler(:kill) do
+    reactor.stop
+  end
 
   puts 'Starting the reactor...'
   reactor.start
@@ -28,16 +76,8 @@ def tcp_echo_server(verbose = true)
   puts 'Done!'
 end
 
-
-
-
-
-
-
-
-
-
-
+###############################################################################
+# DRbDemultiplexer
 
 def kill_drb_echo_server
   there = DRbObject.new_with_uri(DRB_URI)
@@ -73,7 +113,7 @@ def drb_echo_test(count, verbose = true)
 end
 
 def drb_echo_server(verbose = true)
-  demux = Concurrent::DrbDemultiplexer.new(DRB_URI)
+  demux = Concurrent::DRbDemultiplexer.new(DRB_URI)
   reactor = Concurrent::Reactor.new(demux)
 
   count = 0
@@ -94,9 +134,12 @@ def drb_echo_server(verbose = true)
   puts 'Done!'
 end
 
+###############################################################################
+# Console Application - Reactor Test
+
 if __FILE__ == $0
 
-  demux = Concurrent::DrbDemultiplexer.new
+  demux = Concurrent::DRbDemultiplexer.new
   reactor = Concurrent::Reactor.new(demux)
 
   puts "running: #{reactor.running?}"
