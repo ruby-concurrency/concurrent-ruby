9d2ef32208216251252b2d75707faf6d725fe9bd
Jerry D'Antonio <stumpjumper@gmail.com>
2013-10-11 12:59:14 -0400

    Updated all 'channel' references to 'actor' in Actor specs.

--- spec/concurrent/actor_spec.rb
+++ spec/concurrent/actor_spec.rb
@@ -5,19 +5,21 @@ module Concurrent
 
   describe Actor do
 
-    let(:actor_clazz) do
+    let(:actor_class) do
       Class.new(Actor) do
+        attr_reader :last_message
         def initialize(&block)
           @task = block
           super()
         end
-        def act(*message) # :nodoc:
+        def act(*message)
+          @last_message = message
           @task.call(*message) unless @task.nil?
         end
       end
     end
 
-    subject { Class.new(actor_clazz).new }
+    subject { Class.new(actor_class).new }
 
     it_should_behave_like :runnable
 
@@ -35,36 +37,36 @@ module Concurrent
 
       it 'pushes a message onto the queue' do
         @expected = false
-        channel = actor_clazz.new{|msg| @expected = msg }
-        @thread = Thread.new{ channel.run }
+        actor = actor_class.new{|msg| @expected = msg }
+        @thread = Thread.new{ actor.run }
         @thread.join(0.1)
-        channel.post(true)
+        actor.post(true)
         @thread.join(0.1)
         @expected.should be_true
-        channel.stop
+        actor.stop
       end
 
       it 'returns the current size of the queue' do
-        channel = actor_clazz.new{|msg| sleep }
-        @thread = Thread.new{ channel.run }
+        actor = actor_class.new{|msg| sleep }
+        @thread = Thread.new{ actor.run }
         @thread.join(0.1)
-        channel.post(true).should == 1
+        actor.post(true).should == 1
         @thread.join(0.1)
-        channel.post(true).should == 1
+        actor.post(true).should == 1
         @thread.join(0.1)
-        channel.post(true).should == 2
-        channel.stop
+        actor.post(true).should == 2
+        actor.stop
       end
 
       it 'is aliased a <<' do
         @expected = false
-        channel = actor_clazz.new{|msg| @expected = msg }
-        @thread = Thread.new{ channel.run }
+        actor = actor_class.new{|msg| @expected = msg }
+        @thread = Thread.new{ actor.run }
         @thread.join(0.1)
-        channel << true
+        actor << true
         @thread.join(0.1)
         @expected.should be_true
-        channel.stop
+        actor.stop
       end
     end
 
@@ -81,13 +83,13 @@ module Concurrent
     context '#stop' do
 
       it 'empties the queue' do
-        channel = actor_clazz.new{|msg| sleep }
-        @thread = Thread.new{ channel.run }
-        10.times { channel.post(true) }
+        actor = actor_class.new{|msg| sleep }
+        @thread = Thread.new{ actor.run }
+        10.times { actor.post(true) }
         @thread.join(0.1)
-        channel.stop
+        actor.stop
         @thread.join(0.1)
-        q = channel.instance_variable_get(:@queue)
+        q = actor.instance_variable_get(:@queue)
         if q.size >= 1
           q.pop.should == :stop
         else
@@ -109,23 +111,23 @@ module Concurrent
 
       it 'runs the constructor block once for every message' do
         @expected = 0
-        channel = actor_clazz.new{|msg| @expected += 1 }
-        @thread = Thread.new{ channel.run }
+        actor = actor_class.new{|msg| @expected += 1 }
+        @thread = Thread.new{ actor.run }
         @thread.join(0.1)
-        10.times { channel.post(true) }
+        10.times { actor.post(true) }
         @thread.join(0.1)
         @expected.should eq 10
-        channel.stop
+        actor.stop
       end
 
       it 'passes the message to the block' do
         @expected = []
-        channel = actor_clazz.new{|msg| @expected << msg }
-        @thread = Thread.new{ channel.run }
+        actor = actor_class.new{|msg| @expected << msg }
+        @thread = Thread.new{ actor.run }
         @thread.join(0.1)
-        10.times {|i| channel.post(i) }
+        10.times {|i| actor.post(i) }
         @thread.join(0.1)
-        channel.stop
+        actor.stop
         @expected.should eq (0..9).to_a
       end
     end
@@ -133,13 +135,13 @@ module Concurrent
     context 'exception handling' do
 
       it 'supresses exceptions thrown when handling messages' do
-        channel = actor_clazz.new{|msg| raise StandardError }
-        @thread = Thread.new{ channel.run }
+        actor = actor_class.new{|msg| raise StandardError }
+        @thread = Thread.new{ actor.run }
         expect {
           @thread.join(0.1)
-          10.times { channel.post(true) }
+          10.times { actor.post(true) }
         }.not_to raise_error
-        channel.stop
+        actor.stop
       end
     end
 
@@ -163,32 +165,32 @@ module Concurrent
 
       it 'does not notify observers when a message raises an exception' do
         observer.should_not_receive(:update).with(any_args())
-        channel = actor_clazz.new{|msg| raise StandardError }
-        channel.add_observer(observer)
-        @thread = Thread.new{ channel.run }
+        actor = actor_class.new{|msg| raise StandardError }
+        actor.add_observer(observer)
+        @thread = Thread.new{ actor.run }
         @thread.join(0.1)
-        10.times { channel.post(true) }
+        10.times { actor.post(true) }
         @thread.join(0.1)
-        channel.stop
+        actor.stop
       end
 
       it 'passes the time, message, and result to the observer' do
-        channel = actor_clazz.new{|*msg| msg }
-        channel.add_observer(observer)
-        @thread = Thread.new{ channel.run }
+        actor = actor_class.new{|*msg| msg }
+        actor.add_observer(observer)
+        @thread = Thread.new{ actor.run }
         @thread.join(0.1)
-        channel.post(42)
+        actor.post(42)
         @thread.join(0.1)
         observer.notice[0].should be_a(Time)
         observer.notice[1].should == [42]
         observer.notice[2].should == [42]
-        channel.stop
+        actor.stop
       end
     end
 
     context '#pool' do
 
-      let(:clazz){ Class.new(actor_clazz) }
+      let(:clazz){ Class.new(actor_class) }
 
       it 'raises an exception if the count is zero or less' do
         expect {
@@ -196,63 +198,63 @@ module Concurrent
         }.to raise_error(ArgumentError)
       end
   
-      it 'creates the requested number of channels' do
-        mailbox, channels = clazz.pool(5)
-        channels.size.should == 5
+      it 'creates the requested number of actors' do
+        mailbox, actors = clazz.pool(5)
+        actors.size.should == 5
       end
 
-      it 'passes the block to each channel' do
+      it 'passes the block to each actor' do
         block = proc{ nil }
         clazz.should_receive(:new).with(&block)
         clazz.pool(1, &block)
       end
 
-      it 'gives all channels the same mailbox' do
-        mailbox, channels = clazz.pool(2)
-        mbox1 = channels.first.instance_variable_get(:@queue)
-        mbox2 = channels.last.instance_variable_get(:@queue)
+      it 'gives all actors the same mailbox' do
+        mailbox, actors = clazz.pool(2)
+        mbox1 = actors.first.instance_variable_get(:@queue)
+        mbox2 = actors.last.instance_variable_get(:@queue)
         mbox1.should eq mbox2
       end
 
       it 'returns a Poolbox as the first retval' do
-        mailbox, channels = clazz.pool(2)
+        mailbox, actors = clazz.pool(2)
         mailbox.should be_a(Actor::Poolbox)
       end
 
-      it 'gives the Poolbox the same mailbox as the channels' do
-        mailbox, channels = clazz.pool(1)
+      it 'gives the Poolbox the same mailbox as the actors' do
+        mailbox, actors = clazz.pool(1)
         mbox1 = mailbox.instance_variable_get(:@queue)
-        mbox2 = channels.first.instance_variable_get(:@queue)
+        mbox2 = actors.first.instance_variable_get(:@queue)
         mbox1.should eq mbox2
       end
 
-      it 'returns an array of channels as the second retval' do
-        mailbox, channels = clazz.pool(2)
-        channels.each do |channel|
-          channel.should be_a(clazz)
+      it 'returns an array of actors as the second retval' do
+        mailbox, actors = clazz.pool(2)
+        actors.each do |actor|
+          actor.should be_a(clazz)
         end
       end
 
       it 'posts to the mailbox with Poolbox#post' do
         @expected = false
-        mailbox, channels = clazz.pool(1){|msg| @expected = true }
-        @thread = Thread.new{ channels.first.run }
+        mailbox, actors = clazz.pool(1){|msg| @expected = true }
+        @thread = Thread.new{ actors.first.run }
         sleep(0.1)
         mailbox.post(42)
         sleep(0.1)
-        channels.each{|channel| channel.stop }
+        actors.each{|actor| actor.stop }
         @thread.kill
         @expected.should be_true
       end
 
       it 'posts to the mailbox with Poolbox#<<' do
         @expected = false
-        mailbox, channels = clazz.pool(1){|msg| @expected = true }
-        @thread = Thread.new{ channels.first.run }
+        mailbox, actors = clazz.pool(1){|msg| @expected = true }
+        @thread = Thread.new{ actors.first.run }
         sleep(0.1)
         mailbox << 42
         sleep(0.1)
-        channels.each{|channel| channel.stop }
+        actors.each{|actor| actor.stop }
         @thread.kill
         @expected.should be_true
       end
@@ -266,50 +268,41 @@ module Concurrent
 
       context '#pool' do
 
-        it 'creates channels of the appropriate subclass' do
-          actor = Class.new(actor_clazz)
-          mailbox, channels = actor.pool(1)
-          channels.first.should be_a(actor)
+        it 'creates actors of the appropriate subclass' do
+          actor = Class.new(actor_class)
+          mailbox, actors = actor.pool(1)
+          actors.first.should be_a(actor)
         end
       end
 
       context '#act overloading' do
 
-        let(:actor) do
-          Class.new(actor_clazz) {
-            attr_reader :last_message
-            def act(*message)
-              @last_message = message
-            end
-          }
-        end
-
         it 'raises an exception if #act is not implemented in the subclass' do
-          channel = Class.new(Actor).new
-          @thread = Thread.new{ channel.run }
+          actor = Class.new(Actor).new
+          @thread = Thread.new{ actor.run }
           @thread.join(0.1)
           expect {
-            channel.post(:foo)
+            actor.post(:foo)
             @thread.join(0.1)
           }.to raise_error(NotImplementedError)
-          channel.stop
+          actor.stop
         end
 
         it 'uses the subclass #act implementation' do
-          channel = actor.new{|*args| @expected = true }
-          @thread = Thread.new{ channel.run }
+          actor = actor_class.new{|*args| @expected = true }
+          @thread = Thread.new{ actor.run }
           @thread.join(0.1)
-          channel.post(:foo)
+          actor.post(:foo)
           @thread.join(0.1)
-          channel.last_message.should eq [:foo]
-          channel.stop
+          actor.last_message.should eq [:foo]
+          actor.stop
         end
       end
 
       context '#on_error overloading' do
 
-        let(:actor) do
-          Class.new(actor_clazz) {
+        let(:bad_actor) do
+          Class.new(actor_class) {
             attr_reader :last_error
             def act(*message)
               raise StandardError
@@ -321,15 +314,15 @@ module Concurrent
         end
 
         it 'uses the subclass #on_error implementation' do
-          channel = actor.new
-          @thread = Thread.new{ channel.run }
+          actor = bad_actor.new
+          @thread = Thread.new{ actor.run }
           @thread.join(0.1)
-          channel.post(42)
+          actor.post(42)
           @thread.join(0.1)
-          channel.last_error[0].should be_a(Time)
-          channel.last_error[1].should eq [42]
-          channel.last_error[2].should be_a(StandardError)
-          channel.stop
+          actor.last_error[0].should be_a(Time)
+          actor.last_error[1].should eq [42]
+          actor.last_error[2].should be_a(StandardError)
+          actor.stop
         end
       end
     end
@@ -337,45 +330,45 @@ module Concurrent
     context 'supervision' do
 
       it 'can be started by a Supervisor' do
-        channel = actor_clazz.new
+        actor = actor_class.new
         supervisor = Supervisor.new
-        supervisor.add_worker(channel)
+        supervisor.add_worker(actor)
         
-        channel.should_receive(:run).with(no_args())
+        actor.should_receive(:run).with(no_args())
         supervisor.run!
         sleep(0.1)
 
         supervisor.stop
         sleep(0.1)
-        channel.stop
+        actor.stop
       end
 
       it 'can receive messages while under supervision' do
         @expected = false
-        channel = actor_clazz.new{|*args| @expected = true}
+        actor = actor_class.new{|*args| @expected = true}
         supervisor = Supervisor.new
-        supervisor.add_worker(channel)
+        supervisor.add_worker(actor)
         supervisor.run!
         sleep(0.1)
 
-        channel.post(42)
+        actor.post(42)
         sleep(0.1)
         @expected.should be_true
 
         supervisor.stop
         sleep(0.1)
-        channel.stop
+        actor.stop
       end
 
       it 'can be stopped by a supervisor' do
-        channel = actor_clazz.new
+        actor = actor_class.new
         supervisor = Supervisor.new
-        supervisor.add_worker(channel)
+        supervisor.add_worker(actor)
         
         supervisor.run!
         sleep(0.1)
 
-        channel.should_receive(:stop).with(no_args())
+        actor.should_receive(:stop).with(no_args())
         supervisor.stop
         sleep(0.1)
       end
