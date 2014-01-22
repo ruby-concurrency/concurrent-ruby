d7b6037d0d80df9d30aca39aa2ca80b4ce2d8ae4
Jerry D'Antonio <stumpjumper@gmail.com>
2014-01-21 22:36:34 -0500

    ActorClient skeleton now extends Actor

--- lib/concurrent/actor_client.rb
+++ lib/concurrent/actor_client.rb
@@ -1,23 +1,37 @@
+require 'concurrent/actor'
 require 'concurrent/postable'
 
 module Concurrent
 
-  class ActorClient
-    include Postable
+  class ActorClient < Actor
 
     def initialize(recipient, host = 'localhost', port = 8787)
-      # this example pulls from Postable's queue then sends over DRb
-      # the thread would have to be replaced with something less brute-force
-      # it may even be an actor itself...
-      @thread = Thread.new do
-        loop do
-          package = queue.pop
-          # examine the Concurrent::Postable::Package object
-          # send the message over DRb
-          # handle the response
-        end
-      end
     end
 
+    protected
+
+    def act(*message)
+      # send message to ActorServer over DRb
+      # process the result
+      # let Actor do the rest
+    end
   end
+
+  #class ActorClient
+    #include Postable
+
+    #def initialize(recipient, host = 'localhost', port = 8787)
+      ## this example pulls from Postable's queue then sends over DRb
+      ## the thread would have to be replaced with something less brute-force
+      ## it may even be an actor itself...
+      #@thread = Thread.new do
+        #loop do
+          #package = queue.pop
+          ## examine the Concurrent::Postable::Package object
+          ## send the message over DRb
+          ## handle the response
+        #end
+      #end
+    #end
+  #end
 end
