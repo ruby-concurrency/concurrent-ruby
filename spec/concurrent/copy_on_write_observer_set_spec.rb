2aae220ac2a346b6ff1fffcacb1a5adb494bcd17
Michele Della Torre <michele.dellatorre@gmail.com>
2014-02-05 17:07:12 +0100

    Observer set Ruby Observable module is not thread safe, so every class using it is exposed to potential concurrency problems. Two thread safe observer sets have been added, using different strategy: a copy-on-write approach and a simpler copy-on-notify. Both use the open-call pattern for notify to avoid deadlock

---
+++ spec/concurrent/copy_on_write_observer_set_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require_relative 'observer_set_shared'
+
+module Concurrent
+
+  describe CopyOnWriteObserverSet do
+    it_behaves_like 'an observer set'
+
+    it 'should be reviewed'
+
+    context 'notify and delete' do
+      it 'should be reviewed'
+    end
+  end
+
+end
