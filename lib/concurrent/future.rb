8e7f37360cde88da5182e3615cdb70dcd435fbf8
Michele Della Torre <michele.dellatorre@gmail.com>
2014-02-12 20:06:44 +0100

    Future with thread safe observers
    Standard Ruby Observer module is not thread safe and it has been replaced with a thread safe observer set.
    I preferred a copy-on-write set because I don't expect many observers to register, but we can add an option to the constructor to let the user choose the implementation

--- lib/concurrent/future.rb
+++ lib/concurrent/future.rb
@@ -1,20 +1,20 @@
 require 'thread'
-require 'observer'
 
 require 'concurrent/global_thread_pool'
 require 'concurrent/obligation'
+require 'concurrent/copy_on_write_observer_set'
 
 module Concurrent
 
   class Future
     include Obligation
-    include Observable
     include UsesGlobalThreadPool
 
     def initialize(opts = {}, &block)
       raise ArgumentError.new('no block given') unless block_given?
 
       init_mutex
+      @observers = CopyOnWriteObserverSet.new
       @state = :unscheduled
       @task = block
       set_deref_options(opts)
@@ -32,10 +32,10 @@ module Concurrent
             observer.send(f, *args)
           end
         else
-          super
+          @observers.add_observer(observer, func)
         end
       end
-      return func
+      func
     end
 
     def execute
@@ -46,7 +46,7 @@ module Concurrent
       Future.thread_pool.post do
         work(&@task)
       end
-      return self
+      self
     end
 
     def self.execute(opts = {}, &block)
@@ -67,9 +67,7 @@ module Concurrent
         val = self.value
         mutex.synchronize do
           event.set
-          changed
-          notify_observers(Time.now, val, @reason)
-          delete_observers
+          @observers.notify_and_delete_observers(Time.now, val, @reason)
         end
       end
     end
