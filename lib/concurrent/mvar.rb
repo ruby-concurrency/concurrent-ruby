edc1e1d78334b92c4db4c4aa6acab1e78393f655
Chris Seaton <chris@chrisseaton.com>
2014-02-24 10:16:39 +0000

    MVar: initial implementation.

---
+++ lib/concurrent/mvar.rb
@@ -0,0 +1,58 @@
+require 'concurrent/event'
+
+module Concurrent
+
+  class MVar
+
+    EMPTY = Object.new
+    TIMEOUT = Object.new
+
+    def initialize(value=EMPTY)
+      @value = value
+      @mutex = Mutex.new
+      @empty_condition = ConditionVariable.new
+      @full_condition = ConditionVariable.new
+    end
+
+    def take(timeout = nil)
+      @mutex.synchronize do
+        # If the value isn't empty, wait for full to be signalled
+        @full_condition.wait(@mutex, timeout) if empty?
+
+        # If we timed out we'll still be empty
+        if not empty?
+          value = @value
+          @value = EMPTY
+          @empty_condition.signal
+          value
+        else
+          TIMEOUT
+        end
+      end
+
+      
+    end
+
+    def put(value, timeout = nil)
+      @mutex.synchronize do
+        # Unless the value is empty, wait for empty to be signalled
+        @empty_condition.wait(@mutex, timeout) unless empty?
+
+        # If we timed out we won't be empty
+        if empty?
+          @value = value
+          @full_condition.signal
+          value
+        else
+          TIMEOUT
+        end
+      end
+    end
+
+    def empty?
+      @value == EMPTY
+    end
+
+  end
+
+end
---
+++ spec/concurrent/mvar_spec.rb
@@ -0,0 +1,115 @@
+require 'spec_helper'
+
+module Concurrent
+
+  describe MVar do
+
+    context '#initialize' do
+
+      it 'accepts no initial value' do
+        m = MVar.new
+        m.should be_empty
+      end
+
+      it 'accepts an empty initial value' do
+        m = MVar.new(MVar::EMPTY)
+        m.should be_empty
+      end
+
+      it 'accepts an initial value' do
+        m = MVar.new(14)
+        m.should_not be_empty
+      end
+
+      it 'accepts a nil initial value' do
+        m = MVar.new(nil)
+        m.should_not be_empty
+      end
+
+    end
+
+    context '#take' do
+
+      it 'sets the MVar to empty' do
+        m = MVar.new(14)
+        m.take
+        m.should be_empty
+      end
+
+      it 'returns the value on a full MVar' do
+        m = MVar.new(14)
+        m.take.should eq 14
+      end
+
+      it 'waits for another thread to #put' do
+        m = MVar.new
+
+        putter = Thread.new {
+          sleep(0.5)
+          m.put 14 
+        }
+
+        m.take.should eq 14
+      end
+
+      it 'returns TIMEOUT on timeout on an empty MVar' do
+        m = MVar.new
+        m.take(0.5).should eq MVar::TIMEOUT
+      end
+
+    end
+
+    context '#put' do
+
+      it 'sets the MVar to be empty' do
+        m = MVar.new(14)
+        m.take
+        m.should be_empty
+      end
+
+      it 'sets a new value on an empty MVar' do
+        m = MVar.new
+        m.put 14
+        m.take.should eq 14
+      end
+
+      it 'waits for another thread to #take' do
+        m = MVar.new(14)
+
+        putter = Thread.new {
+          sleep(0.5)
+          m.take
+        }
+
+        m.put(14).should eq 14
+      end
+
+      it 'returns TIMEOUT on timeout on a full MVar' do
+        m = MVar.new(14)
+        m.put(14, 0.5).should eq MVar::TIMEOUT
+      end
+
+      it 'returns the value' do
+        m = MVar.new
+        m.put(14).should eq 14
+      end
+
+    end
+
+    context '#empty' do
+
+      it 'returns true on an empty MVar' do
+        m = MVar.new
+        m.should be_empty
+      end
+
+      it 'returns false on a full MVar' do
+        m = MVar.new(14)
+        m.should_not be_empty
+      end
+
+    end
+
+  end
+
+end
