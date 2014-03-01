60fdda6b29da890dc1906a18f6fd055adc849ed0
Jerry D'Antonio <stumpjumper@gmail.com>
2014-03-01 16:28:40 -0500

    Began writing YARD documentation for Channel.

--- lib/concurrent/channel.rb
+++ lib/concurrent/channel.rb
@@ -3,6 +3,21 @@ require 'concurrent/stoppable'
 
 module Concurrent
 
+  # `Channel` is a functional programming variation of `Actor`, based very loosely on the
+  # *MailboxProcessor* agent in F#. `Actor` is used to create objects that receive messages
+  # from other threads then processes those messages based on the behavior of the class.
+  # `Channel` creates objects that receive messages and processe them using the block
+  # given at construction. `Channel` is implemented as a subclass of `Actor` and supports
+  # all message-passing methods of that class. `Channel` also supports pools with a shared
+  # mailbox.
+  #
+  # @see Concurrent::Actor
+  # @see Concurrent::Postable
+  # @see Concurrent::Runnable
+  # @see Concurrent::Stoppable
+  #
+  # @see {http://blogs.msdn.com/b/dsyme/archive/2010/02/15/async-and-parallel-design-patterns-in-f-part-3-agents.aspx Async and Parallel Design Patterns in F#: Agents}
+  # @see {http://msdn.microsoft.com/en-us/library/ee370357.aspx Control.MailboxProcessor<'Msg> Class (F#)}
   class Channel < Actor
     include Stoppable
 
