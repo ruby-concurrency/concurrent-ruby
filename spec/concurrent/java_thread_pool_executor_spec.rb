b564d41afa399b8a46e6ef6a06bb7a404b67c465
Jerry D'Antonio <stumpjumper@gmail.com>
2014-03-31 09:47:01 -0400

    Added tests for thread pool executor classes.

--- spec/concurrent/java_thread_pool_executor_spec.rb
+++ spec/concurrent/java_thread_pool_executor_spec.rb
@@ -1,52 +1,29 @@
-#require 'spec_helper'
-#
-#if jruby?
-#
-#  require_relative 'cached_thread_pool_shared'
-#  require_relative 'fixed_thread_pool_shared'
-#
-#  module Concurrent
-#
-#    describe JavaThreadPoolExecutor do
-#
-#      after(:each) do
-#        subject.kill
-#        sleep(0.1)
-#      end
-#
-#      context 'cached thread pool emulation' do
-#
-#        let(:described_class) do
-#          Class.new(JavaThreadPoolExecutor) do
-#            def initialize(opts = {})
-#              max_length = opts.fetch(:max_threads, JavaThreadPoolExecutor::DEFAULT_MAX_POOL_SIZE).to_i
-#              idletime = opts.fetch(:idletime, JavaThreadPoolExecutor::DEFAULT_THREAD_IDLETIMEOUT).to_i
-#              raise ArgumentError.new('idletime must be greater than zero') if idletime <= 0
-#              raise ArgumentError.new('max_threads must be greater than zero') if max_length <= 0
-#              super(opts)
-#            end
-#          end
-#        end
-#
-#        subject { described_class.new(max_threads: 5) }
-#
-#        it_should_behave_like :cached_thread_pool
-#      end
-#
-#      context 'fixed thread pool emulation' do
-#
-#        let(:described_class) do
-#          Class.new(JavaThreadPoolExecutor) do
-#            def initialize(max_threads)
-#              super(min_threads: max_threads, max_threads: max_threads, idletime: 0)
-#            end
-#          end
-#        end
-#
-#        subject { described_class.new(max_threads: 5) }
-#
-#        it_should_behave_like :fixed_thread_pool
-#      end
-#    end
-#  end
-#end
+require 'spec_helper'
+
+if jruby?
+
+  require_relative 'thread_pool_shared'
+
+  module Concurrent
+
+    describe JavaThreadPoolExecutor do
+
+      after(:each) do
+        subject.kill
+        sleep(0.1)
+      end
+
+      subject do
+        JavaThreadPoolExecutor.new(
+          min_threads: 2,
+          max_threads: 5,
+          idletime: 1,
+          max_queue: 10,
+          overflow_policy: :abort
+        )
+      end
+
+      it_should_behave_like :thread_pool
+    end
+  end
+end
