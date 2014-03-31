60892789391d074b9163eaf9dc592a82868f12c5
Jerry D'Antonio <stumpjumper@gmail.com>
2014-03-31 10:30:13 -0400

    Moved pending specs from thread_pool_shared to thread_pool_executor_shared.

--- spec/concurrent/thread_pool_executor_shared.rb
+++ spec/concurrent/thread_pool_executor_shared.rb
@@ -9,4 +9,69 @@ share_examples_for :thread_pool_executor do
   end
 
   it_should_behave_like :thread_pool
+
+  context '#max_queue' do
+
+    it 'is set to zero by default' do
+      pending
+    end
+
+    it 'returns the set value on creation' do
+      pending
+    end
+
+    it 'returns the set value when running' do
+      pending
+    end
+
+    it 'returns the set value after stopping' do
+      pending
+    end
+  end
+
+  context '#queue_length' do
+
+    it 'returns zero on creation' do
+      pending
+    end
+
+    it 'returns zero when there are no enqueued tasks' do
+      pending
+    end
+
+    it 'returns the size of the queue when tasks are enqueued' do
+      pending
+    end
+
+    it 'returns zero when stopped' do
+      pending
+    end
+  end
+
+  context '#remaining_capacity' do
+
+    it 'returns -1 when :max_queue is set to zero' do
+      pending
+    end
+
+    it 'returns :max_size on creation' do
+      pending
+    end
+
+    it 'returns :max_size when no tasks are enqueued' do
+      pending
+    end
+
+    it 'returns the remaining capacity when tasks are enqueued' do
+      pending
+    end
+
+    it 'returns :max_size when stopped' do
+      pending
+    end
+  end
+
+  context '#overload_policy' do
+    pending
+  end
 end
