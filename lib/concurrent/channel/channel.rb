46531d7bd6a9704f8488fa4e57789f17e5d0e662
Michele Della Torre <michele.dellatorre@gmail.com>
2014-04-02 08:53:07 +0200

    Channel.select removes old probes

--- lib/concurrent/channel/channel.rb
+++ lib/concurrent/channel/channel.rb
@@ -2,9 +2,10 @@ module Concurrent
   class Channel
     def self.select(*channels)
       probe = Probe.new
-
       channels.each { |channel| channel.select(probe) }
-      probe.value
+      result = probe.value
+      channels.each { |channel| channel.remove_probe(probe) }
+      result
     end
   end
 end
--- spec/concurrent/channel/channel_spec.rb
+++ spec/concurrent/channel/channel_spec.rb
@@ -16,6 +16,19 @@ module Concurrent
 
           value.should eq 77
         end
+
+        it 'cleans up' do
+          channels = [ UnbufferedChannel.new, UnbufferedChannel.new]
+          channels.each { |ch| ch.stub(:remove_probe).with( an_instance_of(Probe) )}
+
+          Thread.new { channels[1].push 77 }
+
+          value = Channel.select(*channels)
+
+          value.should eq 77
+
+          channels.each { |ch| expect(ch).to have_received(:remove_probe).with( an_instance_of(Probe) ) }
+        end
       end
 
     end
