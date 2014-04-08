bd63253067d748bde032329719ba195a882dc8d4
Michele Della Torre <michele.dellatorre@gmail.com>
2014-04-08 08:17:39 +0200

    added exchanger internal doc

--- lib/concurrent/exchanger.rb
+++ lib/concurrent/exchanger.rb
@@ -8,6 +8,9 @@ module Concurrent
       @second = MVar.new(MVar::EMPTY, opts)
     end
 
+    # @param [Object] value the value to exchange with an other thread
+    # @param [Numeric] timeout the maximum time in second to wait for one other thread. nil (default value) means no timeout
+    # @return [Object] the value exchanged by the other thread; nil if timed out
     def exchange(value, timeout = nil)
       first = @first.take(timeout)
       if first == MVar::TIMEOUT
