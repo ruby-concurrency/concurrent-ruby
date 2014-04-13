dc1d9e89ffe867fa4e843eeb5f6bdd593a113cbf
Jerry D'Antonio <stumpjumper@gmail.com>
2014-04-13 18:02:32 -0400

    Implemented locking on CAtomicFixnum.

--- ext/concurrent_ruby/atomic_fixnum.c
+++ ext/concurrent_ruby/atomic_fixnum.c
@@ -14,71 +14,99 @@ void atomic_fixnum_deallocate(void*);
 
 VALUE atomic_fixnum_allocate(VALUE klass)
 {
-  CAtomicFixnum* sval = malloc(sizeof(CAtomicFixnum));
-  return Data_Wrap_Struct(klass, NULL, atomic_fixnum_deallocate, sval);
-  // CAtomicFixnum* sval;
-  // return Data_Make_Struct(klass, CAtomicFixnum, NULL, atomic_fixnum_deallocate, sval);
+  CAtomicFixnum* atomic;
+  VALUE sval = Data_Make_Struct(klass, CAtomicFixnum, NULL, atomic_fixnum_deallocate, atomic);
+
+  pthread_mutex_init(&atomic->mutex, NULL);
+
+  return(sval);
 }
 
 void atomic_fixnum_deallocate(void* sval)
 {
-  free((CAtomicFixnum*) sval);
+  CAtomicFixnum* atomic = (CAtomicFixnum*) sval;
+
+  pthread_mutex_destroy(&atomic->mutex);
+
+  free(atomic);
 }
 
 VALUE method_atomic_fixnum_initialize(int argc, VALUE* argv, VALUE self) {
   rb_check_arity(argc, 0, 1);
   if (argc == 1) Check_Type(argv[0], T_FIXNUM);
 
-  CAtomicFixnum* sval;
-  Data_Get_Struct(self, CAtomicFixnum, sval);
+  CAtomicFixnum* atomic;
+  Data_Get_Struct(self, CAtomicFixnum, atomic);
 
-  sval->value = (argc == 1 ? FIX2INT(argv[0]) : 0);
+  atomic->value = (argc == 1 ? FIX2INT(argv[0]) : 0);
   return(self);
 }
 
 VALUE method_atomic_fixnum_value(VALUE self) {
-  CAtomicFixnum* sval;
-  Data_Get_Struct(self, CAtomicFixnum, sval);
-  return(INT2FIX(sval->value));
+  CAtomicFixnum* atomic;
+  VALUE value;
+
+  Data_Get_Struct(self, CAtomicFixnum, atomic);
+
+  pthread_mutex_lock(&atomic->mutex);
+  value = INT2FIX(atomic->value);
+  pthread_mutex_unlock(&atomic->mutex);
+
+  return(value);
 }
 
 VALUE method_atomic_fixnum_value_eq(VALUE self, VALUE value) {
   Check_Type(value, T_FIXNUM);
-  CAtomicFixnum* sval;
-  Data_Get_Struct(self, CAtomicFixnum, sval);
-  sval->value = FIX2INT(value);
+
+  CAtomicFixnum* atomic;
+  Data_Get_Struct(self, CAtomicFixnum, atomic);
+
+  pthread_mutex_lock(&atomic->mutex);
+  atomic->value = FIX2INT(value);
+  pthread_mutex_unlock(&atomic->mutex);
+
   return(value);
 }
 
 VALUE method_atomic_fixnum_increment(VALUE self) {
-  CAtomicFixnum* sval;
-  Data_Get_Struct(self, CAtomicFixnum, sval);
-  sval->value++;
-  return(INT2FIX(sval->value));
+  CAtomicFixnum* atomic;
+  Data_Get_Struct(self, CAtomicFixnum, atomic);
+
+  pthread_mutex_lock(&atomic->mutex);
+  atomic->value++;
+  pthread_mutex_unlock(&atomic->mutex);
+
+  return(INT2FIX(atomic->value));
 }
 
 VALUE method_atomic_fixnum_decrement(VALUE self) {
-  CAtomicFixnum* sval;
-  Data_Get_Struct(self, CAtomicFixnum, sval);
-  sval->value--;
-  return(INT2FIX(sval->value));
+  CAtomicFixnum* atomic;
+  Data_Get_Struct(self, CAtomicFixnum, atomic);
+
+  pthread_mutex_lock(&atomic->mutex);
+  atomic->value--;
+  pthread_mutex_unlock(&atomic->mutex);
+
+  return(INT2FIX(atomic->value));
 }
 
 VALUE method_atomic_fixnum_compare_and_set(VALUE self, VALUE rb_expect, VALUE rb_update) {
   Check_Type(rb_expect, T_FIXNUM);
   Check_Type(rb_update, T_FIXNUM);
 
-  CAtomicFixnum* sval;
-  Data_Get_Struct(self, CAtomicFixnum, sval);
+  CAtomicFixnum* atomic;
+  Data_Get_Struct(self, CAtomicFixnum, atomic);
 
   long expect = FIX2INT(rb_expect);
   long update = FIX2INT(rb_update);
   VALUE retval = Qfalse;
 
-  if (sval->value == expect) {
-    sval->value = update;
+  pthread_mutex_lock(&atomic->mutex);
+  if (atomic->value == expect) {
+    atomic->value = update;
     retval = Qtrue;
   }
+  pthread_mutex_unlock(&atomic->mutex);
 
   return(retval);
 }
