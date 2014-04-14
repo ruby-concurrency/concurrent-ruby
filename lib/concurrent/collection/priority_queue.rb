f930550b42c7635810bf8142e76ebeca26a065f1
Jerry D'Antonio <stumpjumper@gmail.com>
2014-04-14 09:38:03 -0400

    Fixed documentation for PriorityQueue.

--- lib/concurrent/collection/priority_queue.rb
+++ lib/concurrent/collection/priority_queue.rb
@@ -1,60 +1,66 @@
 module Concurrent
 
-  # @!macro [new] priority_queue
+  # @!macro [attach] priority_queue
   #
-  # A queue collection in which the elements are sorted based on their
-  # comparison (spaceship) operator `<=>`. Items are added to the queue
-  # at a position relative to their priority. On removal the element
-  # with the "highest" priority is removed. By default the sort order is
-  # from highest to lowest, but a lowest-to-highest sort order can be
-  # set on construction.
+  #   A queue collection in which the elements are sorted based on their
+  #   comparison (spaceship) operator `<=>`. Items are added to the queue
+  #   at a position relative to their priority. On removal the element
+  #   with the "highest" priority is removed. By default the sort order is
+  #   from highest to lowest, but a lowest-to-highest sort order can be
+  #   set on construction.
   #
-  # The API is based on the `Queue` class from the Ruby standard library.
+  #   The API is based on the `Queue` class from the Ruby standard library.
   #
-  # The pure Ruby implementation, `MutexPriorityQueue` uses a heap algorithm
-  # stored in an array. The algorithm is based on the work of Robert Sedgewick
-  # and Kevin Wayne.
+  #   The pure Ruby implementation, `MutexPriorityQueue` uses a heap algorithm
+  #   stored in an array. The algorithm is based on the work of Robert Sedgewick
+  #   and Kevin Wayne.
   #
-  # The JRuby native implementation is a thin wrapper around the standard
-  # library `java.util.PriorityQueue`.
+  #   The JRuby native implementation is a thin wrapper around the standard
+  #   library `java.util.PriorityQueue`.
   #
-  # When running under JRuby the class `PriorityQueue` extends `JavaPriorityQueue`.
-  # When running under all other interpreters it extends `MutexPriorityQueue`.
+  #   When running under JRuby the class `PriorityQueue` extends `JavaPriorityQueue`.
+  #   When running under all other interpreters it extends `MutexPriorityQueue`.
   #
-  # @note This implementation is *not* thread safe and performs no blocking.
+  #   @note This implementation is *not* thread safe and performs no blocking.
   #
-  # @see http://en.wikipedia.org/wiki/Priority_queue
-  # @see http://ruby-doc.org/stdlib-2.0.0/libdoc/thread/rdoc/Queue.html
+  #   @see http://en.wikipedia.org/wiki/Priority_queue
+  #   @see http://ruby-doc.org/stdlib-2.0.0/libdoc/thread/rdoc/Queue.html
   #
-  # @see http://algs4.cs.princeton.edu/24pq/index.php#2.6
-  # @see http://algs4.cs.princeton.edu/24pq/MaxPQ.java.html
+  #   @see http://algs4.cs.princeton.edu/24pq/index.php#2.6
+  #   @see http://algs4.cs.princeton.edu/24pq/MaxPQ.java.html
   #
-  # @see http://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
+  #   @see http://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
   class MutexPriorityQueue
 
-    # Create a new priority queue with no items.
+    # @!macro [attach] priority_queue_method_initialize
     #
-    # @param [Hash] opts the options for creating the queue
-    # @option opts [Symbol] :order (:max) dictates the order in which items are
-    #   stored: from highest to lowest when `:max` or `:high`; from lowest to
-    #   highest when `:min` or `:low`
+    #   Create a new priority queue with no items.
+    #  
+    #   @param [Hash] opts the options for creating the queue
+    #   @option opts [Symbol] :order (:max) dictates the order in which items are
+    #     stored: from highest to lowest when `:max` or `:high`; from lowest to
+    #     highest when `:min` or `:low`
     def initialize(opts = {})
       order = opts.fetch(:order, :max)
       @comparator = [:min, :low].include?(order) ? -1 : 1
       clear
     end
 
-    # Removes all of the elements from this priority queue.
+    # @!macro [attach] priority_queue_method_clear
+    #
+    #   Removes all of the elements from this priority queue.
     def clear
       @queue = [nil]
       @length = 0
       true
     end
 
-    # Deletes all items from `self` that are equal to `item`.
+    # @!macro [attach] priority_queue_method_delete
     #
-    # @param [Object] item the item to be removed from the queue
-    # @return [Object] true if the item is found else false
+    #   Deletes all items from `self` that are equal to `item`.
+    #  
+    #   @param [Object] item the item to be removed from the queue
+    #   @return [Object] true if the item is found else false
     def delete(item)
       original_length = @length
       k = 1
@@ -71,44 +77,54 @@ module Concurrent
       @length != original_length
     end
 
-    # Returns `true` if `self` contains no elements.
-    #
-    # @return [Boolean] true if there are no items in the queue else false
+    # @!macro [attach] priority_queue_method_empty
+    #  
+    #   Returns `true` if `self` contains no elements.
+    #  
+    #   @return [Boolean] true if there are no items in the queue else false
     def empty?
       size == 0
     end
 
-    # Returns `true` if the given item is present in `self` (that is, if any
-    # element == `item`), otherwise returns false.
+    # @!macro [attach] priority_queue_method_include
     #
-    # @param [Object] item the item to search for
-    #
-    # @return [Boolean] true if the item is found else false
+    #   Returns `true` if the given item is present in `self` (that is, if any
+    #   element == `item`), otherwise returns false.
+    #  
+    #   @param [Object] item the item to search for
+    #  
+    #   @return [Boolean] true if the item is found else false
     def include?(item)
       @queue.include?(item)
     end
     alias_method :has_priority?, :include?
 
-    # The current length of the queue.
-    #
-    # @return [Fixnum] the number of items in the queue
+    # @!macro [attach] priority_queue_method_length
+    #  
+    #   The current length of the queue.
+    #  
+    #   @return [Fixnum] the number of items in the queue
     def length
       @length
     end
     alias_method :size, :length
 
-    # Retrieves, but does not remove, the head of this queue, or returns `nil`
-    # if this queue is empty.
-    # 
-    # @return [Object] the head of the queue or `nil` when empty
+    # @!macro [attach] priority_queue_method_peek
+    #  
+    #   Retrieves, but does not remove, the head of this queue, or returns `nil`
+    #   if this queue is empty.
+    #   
+    #   @return [Object] the head of the queue or `nil` when empty
     def peek
       @queue[1]
     end
 
-    # Retrieves and removes the head of this queue, or returns `nil` if this
-    # queue is empty.
-    # 
-    # @return [Object] the head of the queue or `nil` when empty
+    # @!macro [attach] priority_queue_method_pop
+    #  
+    #   Retrieves and removes the head of this queue, or returns `nil` if this
+    #   queue is empty.
+    #   
+    #   @return [Object] the head of the queue or `nil` when empty
     def pop
       max = @queue[1]
       swap(1, @length)
@@ -120,9 +136,11 @@ module Concurrent
     alias_method :deq, :pop
     alias_method :shift, :pop
 
-    # Inserts the specified element into this priority queue.
-    #
-    # @param [Object] item the item to insert onto the queue
+    # @!macro [attach] priority_queue_method_push
+    #  
+    #   Inserts the specified element into this priority queue.
+    #  
+    #   @param [Object] item the item to insert onto the queue
     def push(item)
       @length += 1
       @queue << item
@@ -132,12 +150,14 @@ module Concurrent
     alias_method :<<, :push
     alias_method :enq, :push
 
-    # Create a new priority queue from the given list.
-    #
-    # @param [Enumerable] list the list to build the queue from
-    # @param [Hash] opts the options for creating the queue
-    #
-    # @return [PriorityQueue] the newly created and populated queue
+    # @!macro [attach] priority_queue_method_from_list
+    #  
+    #   Create a new priority queue from the given list.
+    #  
+    #   @param [Enumerable] list the list to build the queue from
+    #   @param [Hash] opts the options for creating the queue
+    #  
+    #   @return [PriorityQueue] the newly created and populated queue
     def self.from_list(list, opts = {})
       queue = new(opts)
       list.each{|item| queue << item }
@@ -204,12 +224,7 @@ module Concurrent
     # @!macro priority_queue
     class JavaPriorityQueue
 
-      # Create a new priority queue with no items.
-      #
-      # @param [Hash] opts the options for creating the queue
-      # @option opts [Symbol] :order (:max) dictates the order in which items are
-      #   stored: from highest to lowest when `:max` or `:high`; from lowest to
-      #   highest when `:min` or `:low`
+      # @!macro priority_queue_method_initialize
       def initialize(opts = {})
         order = opts.fetch(:order, :max)
         if [:min, :low].include?(order)
@@ -219,16 +234,13 @@ module Concurrent
         end
       end
 
-      # Removes all of the elements from this priority queue.
+      # @!macro priority_queue_method_clear
       def clear
         @queue.clear
         true
       end
 
-      # Deletes all items from `self` that are equal to `item`.
-      #
-      # @param [Object] item the item to be removed from the queue
-      # @return [Object] true if the item is found else false
+      # @!macro priority_queue_method_delete
       def delete(item)
         found = false
         while @queue.remove(item) do
@@ -237,65 +249,43 @@ module Concurrent
         found
       end
 
-      # Returns `true` if `self` contains no elements.
-      #
-      # @return [Boolean] true if there are no items in the queue else false
+      # @!macro priority_queue_method_empty
       def empty?
         @queue.size == 0
       end
 
-      # Returns `true` if the given item is present in `self` (that is, if any
-      # element == `item`), otherwise returns false.
-      #
-      # @param [Object] item the item to search for
-      #
-      # @return [Boolean] true if the item is found else false
+      # @!macro priority_queue_method_include
       def include?(item)
         @queue.contains(item)
       end
       alias_method :has_priority?, :include?
 
-      # The current length of the queue.
-      #
-      # @return [Fixnum] the number of items in the queue
+      # @!macro priority_queue_method_length
       def length
         @queue.size
       end
       alias_method :size, :length
 
-      # Retrieves, but does not remove, the head of this queue, or returns `nil`
-      # if this queue is empty.
-      # 
-      # @return [Object] the head of the queue or `nil` when empty
+      # @!macro priority_queue_method_peek
       def peek
         @queue.peek
       end
 
-      # Retrieves and removes the head of this queue, or returns `nil` if this
-      # queue is empty.
-      # 
-      # @return [Object] the head of the queue or `nil` when empty
+      # @!macro priority_queue_method_pop
       def pop
         @queue.poll
       end
       alias_method :deq, :pop
       alias_method :shift, :pop
 
-      # Inserts the specified element into this priority queue.
-      #
-      # @param [Object] item the item to insert onto the queue
+      # @!macro priority_queue_method_push
       def push(item)
         @queue.add(item)
       end
       alias_method :<<, :push
       alias_method :enq, :push
 
-      # Create a new priority queue from the given list.
-      #
-      # @param [Enumerable] list the list to build the queue from
-      # @param [Hash] opts the options for creating the queue
-      #
-      # @return [PriorityQueue] the newly created and populated queue
+      # @!macro priority_queue_method_from_list
       def self.from_list(list, opts = {})
         queue = new(opts)
         list.each{|item| queue << item }
