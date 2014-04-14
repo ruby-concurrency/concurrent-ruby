ddaeceaf1a2d940f171004797a46d7f253267ded
Jerry D'Antonio <stumpjumper@gmail.com>
2014-04-14 08:52:51 -0400

    Refactored AtomicFixnum as subclass/superclass rather than module include.

--- lib/concurrent/atomic/atomic_fixnum.rb
+++ lib/concurrent/atomic/atomic_fixnum.rb
@@ -1,7 +1,42 @@
 module Concurrent
 
-  # @!visibility private
-  module MutexAtomicFixnum # :nodoc:
+  # @!macro [attach] atomic_fixnum
+  #
+  #   A numeric value that can be updated atomically. Reads and writes to an atomic
+  #   fixnum and thread-safe and guaranteed to succeed. Reads and writes may block
+  #   briefly but no explicit locking is required.
+  #
+  #   @!method value()
+  #     Retrieves the current `Fixnum` value
+  #     @return [Fixnum] the current value
+  #
+  #   @!method value=(value)
+  #     Explicitly sets the value
+  #     @param [Fixnum] value the new value to be set
+  #     @return [Fixnum] the current value
+  #     @raise [ArgumentError] if the new value is not a `Fixnum`
+  #
+  #   @!method increment()
+  #     Increases the current value by 1
+  #     @return [Fixnum] the current value after incrementation
+  #
+  #   @!method decrement()
+  #     Decreases the current value by 1
+  #     @return [Fixnum] the current value after decrementation
+  #
+  #   @since 0.5.0
+  #   @see http://docs.oracle.com/javase/7/docs/api/java/util/concurrent/atomic/AtomicLong.html java.util.concurrent.atomic.AtomicLong
+  class MutexAtomicFixnum
+
+    # Creates a new `AtomicFixnum` with the given initial value.
+    #
+    # @param [Fixnum] init the initial value
+    # @raise [ArgumentError] if the initial value is not a `Fixnum`
+    def initialize(init = 0)
+      raise ArgumentError.new('initial value must be a Fixnum') unless init.is_a?(Fixnum)
+      @value = init
+      @mutex = Mutex.new
+    end
 
     def allocate_storage(init)
       @value = init
@@ -26,12 +61,14 @@ module Concurrent
         @value += 1
       end
     end
+    alias_method :up, :increment
 
     def decrement
       @mutex.synchronize do
         @value -= 1
       end
     end
+    alias_method :down, :decrement
 
     def compare_and_set(expect, update)
       @mutex.synchronize do
@@ -45,77 +82,54 @@ module Concurrent
     end
   end
 
-  # @!visibility private
-  module JavaAtomicFixnum # :nodoc:
+  if RUBY_PLATFORM == 'java'
 
-    def allocate_storage(init)
-      @atomic = java.util.concurrent.atomic.AtomicLong.new(init)
+    class JavaAtomicFixnum
+
+      # Creates a new `AtomicFixnum` with the given initial value.
+      #
+      # @param [Fixnum] init the initial value
+      # @raise [ArgumentError] if the initial value is not a `Fixnum`
+      def initialize(init = 0)
+        raise ArgumentError.new('initial value must be a Fixnum') unless init.is_a?(Fixnum)
+        @atomic = java.util.concurrent.atomic.AtomicLong.new(init)
+      end
+
+      def allocate_storage(init)
+      end
+
+      def value
+        @atomic.get
+      end
+
+      def value=(value)
+        raise ArgumentError.new('value must be a Fixnum') unless value.is_a?(Fixnum)
+        @atomic.set(value)
+      end
+
+      def increment
+        @atomic.increment_and_get
+      end
+      alias_method :up, :increment
+
+      def decrement
+        @atomic.decrement_and_get
+      end
+      alias_method :down, :decrement
+
+      def compare_and_set(expect, update)
+        @atomic.compare_and_set(expect, update)
+      end
     end
 
-    def value
-      @atomic.get
+    # @!macro atomic_fixnum
+    class AtomicFixnum < JavaAtomicFixnum
     end
 
-    def value=(value)
-      raise ArgumentError.new('value must be a Fixnum') unless value.is_a?(Fixnum)
-      @atomic.set(value)
+  else
+
+    # @!macro atomic_fixnum
+    class AtomicFixnum < MutexAtomicFixnum
     end
-
-    def increment
-      @atomic.increment_and_get
-    end
-
-    def decrement
-      @atomic.decrement_and_get
-    end
-
-    def compare_and_set(expect, update)
-      @atomic.compare_and_set(expect, update)
-    end
-  end
-
-  # A numeric value that can be updated atomically. Reads and writes to an atomic
-  # fixnum and thread-safe and guaranteed to succeed. Reads and writes may block
-  # briefly but no explicit locking is required.
-  #
-  # @!method value()
-  #   Retrieves the current `Fixnum` value
-  #   @return [Fixnum] the current value
-  #
-  # @!method value=(value)
-  #   Explicitly sets the value
-  #   @param [Fixnum] value the new value to be set
-  #   @return [Fixnum] the current value
-  #   @raise [ArgumentError] if the new value is not a `Fixnum`
-  #
-  # @!method increment()
-  #   Increases the current value by 1
-  #   @return [Fixnum] the current value after incrementation
-  #
-  # @!method decrement()
-  #   Decreases the current value by 1
-  #   @return [Fixnum] the current value after decrementation
-  #
-  # @since 0.5.0
-  # @see http://docs.oracle.com/javase/7/docs/api/java/util/concurrent/atomic/AtomicLong.html java.util.concurrent.atomic.AtomicLong
-  class AtomicFixnum
-
-    # Creates a new `AtomicFixnum` with the given initial value.
-    #
-    # @param [Fixnum] init the initial value
-    # @raise [ArgumentError] if the initial value is not a `Fixnum`
-    def initialize(init = 0)
-      raise ArgumentError.new('initial value must be a Fixnum') unless init.is_a?(Fixnum)
-      allocate_storage(init)
-    end
-
-    if RUBY_PLATFORM == 'java'
-      include JavaAtomicFixnum
-    else
-      include MutexAtomicFixnum
-    end
-
-    alias_method :up, :increment
-    alias_method :down, :decrement
   end
 end
--- spec/concurrent/atomic/atomic_fixnum_spec.rb
+++ spec/concurrent/atomic/atomic_fixnum_spec.rb
@@ -1,201 +1,181 @@
 require 'spec_helper'
 
+share_examples_for :atomic_fixnum do
+
+  context 'construction' do
+
+    it 'sets the initial value' do
+      described_class.new(10).value.should eq 10
+    end
+
+    it 'defaults the initial value to zero' do
+      described_class.new.value.should eq 0
+    end
+
+    it 'raises en exception if the initial value is not a Fixnum' do
+      lambda {
+        described_class.new(10.01)
+      }.should raise_error
+    end
+  end
+
+  context '#value' do
+
+    it 'returns the current value' do
+      counter = described_class.new(10)
+      counter.value.should eq 10
+      counter.increment
+      counter.value.should eq 11
+      counter.decrement
+      counter.value.should eq 10
+    end
+  end
+
+  context '#value=' do
+
+    it 'sets the #value to the given `Fixnum`' do
+      atomic = described_class.new(0)
+      atomic.value = 10
+      atomic.value.should eq 10
+    end
+
+    it 'returns the new value' do
+      atomic = described_class.new(0)
+      (atomic.value = 10).should eq 10
+    end
+
+    it 'raises and exception if the value is not a `Fixnum`' do
+      atomic = described_class.new(0)
+      expect {
+        atomic.value = 'foo'
+      }.to raise_error
+    end
+  end
+
+  context '#increment' do
+
+    it 'increases the value by one' do
+      counter = described_class.new(10)
+      3.times{ counter.increment }
+      counter.value.should eq 13
+    end
+
+    it 'returns the new value' do
+      counter = described_class.new(10)
+      counter.increment.should eq 11
+    end
+
+    it 'is aliased as #up' do
+      described_class.new(10).up.should eq 11
+    end
+  end
+
+  context '#decrement' do
+
+    it 'decreases the value by one' do
+      counter = described_class.new(10)
+      3.times{ counter.decrement }
+      counter.value.should eq 7
+    end
+
+    it 'returns the new value' do
+      counter = described_class.new(10)
+      counter.decrement.should eq 9
+    end
+
+    it 'is aliased as #down' do
+      described_class.new(10).down.should eq 9
+    end
+  end
+
+  context '#compare_and_set' do
+
+    it 'returns false if the value is not found' do
+      described_class.new(14).compare_and_set(2, 14).should eq false
+    end
+
+    it 'returns true if the value is found' do
+      described_class.new(14).compare_and_set(14, 2).should eq true
+    end
+
+    it 'sets if the value is found' do
+      f = described_class.new(14)
+      f.compare_and_set(14, 2)
+      f.value.should eq 2
+    end
+
+    it 'does not set if the value is not found' do
+      f = described_class.new(14)
+      f.compare_and_set(2, 12)
+      f.value.should eq 14
+    end
+  end
+end
+
 module Concurrent
 
+  describe MutexAtomicFixnum do
+
+    it_should_behave_like :atomic_fixnum
+
+    specify 'construction is synchronized' do
+      mutex = double('mutex')
+      Mutex.should_receive(:new).once.with(no_args).and_return(mutex)
+      described_class.new
+    end
+
+    specify 'value is synchronized' do
+      mutex = double('mutex')
+      Mutex.stub(:new).with(no_args).and_return(mutex)
+      mutex.should_receive(:synchronize)
+      described_class.new.value
+    end
+
+    specify 'value= is synchronized' do
+      mutex = double('mutex')
+      Mutex.stub(:new).with(no_args).and_return(mutex)
+      mutex.should_receive(:synchronize)
+      described_class.new.value = 10
+    end
+
+    specify 'increment is synchronized' do
+      mutex = double('mutex')
+      Mutex.stub(:new).with(no_args).and_return(mutex)
+      mutex.should_receive(:synchronize)
+      described_class.new.increment
+    end
+
+    specify 'decrement is synchronized' do
+      mutex = double('mutex')
+      Mutex.stub(:new).with(no_args).and_return(mutex)
+      mutex.should_receive(:synchronize)
+      described_class.new.decrement
+    end
+
+    specify 'compare_and_set is synchronized' do
+      mutex = double('mutex')
+      Mutex.stub(:new).with(no_args).and_return(mutex)
+      mutex.should_receive(:synchronize)
+      described_class.new(14).compare_and_set(14, 2)
+    end
+  end
+
+  if jruby?
+
+    describe JavaAtomicFixnum do
+      it_should_behave_like :atomic_fixnum
+    end
+  end
+
   describe AtomicFixnum do
-
-    context 'construction' do
-
-      it 'sets the initial value' do
-        AtomicFixnum.new(10).value.should eq 10
+    if jruby?
+      it 'inherits from JavaAtomicFixnum' do
+        AtomicFixnum.ancestors.should include(JavaAtomicFixnum)
       end
-
-      it 'defaults the initial value to zero' do
-        AtomicFixnum.new.value.should eq 0
-      end
-
-      it 'raises en exception if the initial value is not a Fixnum' do
-        lambda {
-          AtomicFixnum.new(10.01)
-        }.should raise_error(ArgumentError)
-      end
-
-      if jruby?
-
-        it 'uses Java AtomicLong' do
-          mutex = double('mutex')
-          Mutex.stub(:new).with(no_args).and_return(mutex)
-          mutex.should_not_receive(:synchronize)
-          AtomicFixnum.new.value
-        end
-
-      else
-
-        it 'is synchronized' do
-          mutex = double('mutex')
-          Mutex.stub(:new).with(no_args).and_return(mutex)
-          mutex.should_receive(:synchronize)
-          AtomicFixnum.new.value
-        end
-
-      end
-
-    end
-
-    context '#value' do
-
-      it 'returns the current value' do
-        counter = AtomicFixnum.new(10)
-        counter.value.should eq 10
-        counter.increment
-        counter.value.should eq 11
-        counter.decrement
-        counter.value.should eq 10
+    else
+      it 'inherits from MutexAtomicFixnum' do
+        AtomicFixnum.ancestors.should include(MutexAtomicFixnum)
       end
     end
-
-    context '#value=' do
-
-      it 'sets the #value to the given `Fixnum`' do
-        atomic = AtomicFixnum.new(0)
-        atomic.value = 10
-        atomic.value.should eq 10
-      end
-
-      it 'returns the new value' do
-        atomic = AtomicFixnum.new(0)
-        (atomic.value = 10).should eq 10
-      end
-
-      it 'raises and exception if the value is not a `Fixnum`' do
-        atomic = AtomicFixnum.new(0)
-        expect {
-          atomic.value = 'foo'
-        }.to raise_error(ArgumentError)
-      end
-    end
-
-    context '#increment' do
-
-      it 'increases the value by one' do
-        counter = AtomicFixnum.new(10)
-        3.times{ counter.increment }
-        counter.value.should eq 13
-      end
-
-      it 'returns the new value' do
-        counter = AtomicFixnum.new(10)
-        counter.increment.should eq 11
-      end
-
-      it 'is aliased as #up' do
-        AtomicFixnum.new(10).up.should eq 11
-      end
-
-      if jruby?
-
-        it 'does not use Mutex class' do
-          mutex = double('mutex')
-          Mutex.stub(:new).with(no_args).and_return(mutex)
-          mutex.should_not_receive(:synchronize)
-          AtomicFixnum.new.increment
-        end
-
-      else
-
-        it 'is synchronized' do
-          mutex = double('mutex')
-          Mutex.stub(:new).with(no_args).and_return(mutex)
-          mutex.should_receive(:synchronize)
-          AtomicFixnum.new.increment
-        end
-
-      end
-
-    end
-
-    context '#decrement' do
-
-      it 'decreases the value by one' do
-        counter = AtomicFixnum.new(10)
-        3.times{ counter.decrement }
-        counter.value.should eq 7
-      end
-
-      it 'returns the new value' do
-        counter = AtomicFixnum.new(10)
-        counter.decrement.should eq 9
-      end
-
-      it 'is aliased as #down' do
-        AtomicFixnum.new(10).down.should eq 9
-      end
-
-      if jruby?
-
-        it 'does not use Mutex class' do
-          mutex = double('mutex')
-          Mutex.stub(:new).with(no_args).and_return(mutex)
-          mutex.should_not_receive(:synchronize)
-          AtomicFixnum.new.decrement
-        end
-
-      else
-
-        it 'is synchronized' do
-          mutex = double('mutex')
-          Mutex.stub(:new).with(no_args).and_return(mutex)
-          mutex.should_receive(:synchronize)
-          AtomicFixnum.new.decrement
-        end
-
-      end
-
-    end
-
-    context '#compare_and_set' do
-
-      it 'returns false if the value is not found' do
-        AtomicFixnum.new(14).compare_and_set(2, 14).should eq false
-      end
-
-      it 'returns true if the value is found' do
-        AtomicFixnum.new(14).compare_and_set(14, 2).should eq true
-      end
-
-      it 'sets if the value is found' do
-        f = AtomicFixnum.new(14)
-        f.compare_and_set(14, 2)
-        f.value.should eq 2
-      end
-
-      it 'does not set if the value is not found' do
-        f = AtomicFixnum.new(14)
-        f.compare_and_set(2, 12)
-        f.value.should eq 14
-      end
-
-      if jruby?
-
-        it 'does not use Mutex class' do
-          mutex = double('mutex')
-          Mutex.stub(:new).with(no_args).and_return(mutex)
-          mutex.should_not_receive(:synchronize)
-          AtomicFixnum.new(14).compare_and_set(14, 2)
-        end
-
-      else
-
-        it 'is synchronized' do
-          mutex = double('mutex')
-          Mutex.stub(:new).with(no_args).and_return(mutex)
-          mutex.should_receive(:synchronize)
-          AtomicFixnum.new(14).compare_and_set(14, 2)
-        end
-
-      end
-
-    end
-
   end
 end
