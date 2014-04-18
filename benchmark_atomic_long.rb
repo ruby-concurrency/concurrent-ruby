96d8c18ae2cc4b1a8c05766f07f55e43a1b2f8da
Jerry D'Antonio <stumpjumper@gmail.com>
2014-04-18 10:58:38 -0400

    Benchmarked GCC atomic long vs. pthread.

--- benchmark_atomic_long.rb
+++ benchmark_atomic_long.rb
@@ -1,3 +1,5 @@
+$:.push File.join(File.dirname(__FILE__), 'lib')
+
 require 'concurrent'
 require 'benchmark'
 
@@ -8,20 +10,17 @@ def atomic_test(clazz, opts = {})
   num = clazz.new
   latch = Concurrent::CountDownLatch.new(threads)
 
+  print "Testing with #{clazz}...\n"
   stats = Benchmark.measure do
     threads.times do |i|
       Thread.new do
-        print "Starting thread #{i+1}...\n"
         tests.times{ num.up }
         latch.count_down
-        print "Thread #{i+1} done.\n"
       end
     end
-
-    print "Waitning for the latch...\n"
     latch.wait
   end
-  print "#{stats}\n"
+  print stats
 end
 
 atomic_test(Concurrent::MutexAtomicFixnum, threads: 10, tests: 1_000_000)
@@ -37,9 +36,22 @@ end
 # Memory 8 GB 1067 MHz DDR3
 
 # ruby 2.1.1p76 (2014-02-24 revision 45161) [x86_64-darwin13.0]
-#=> 23.050000  62.430000  85.480000 ( 60.877730)
-#=> 1.290000   0.000000   1.290000 (  1.295110)
+#
+# Testing with Concurrent::MutexAtomicFixnum...
+#=> 21.180000  55.370000  76.550000 ( 54.700031)
+#
+# Testing with Concurrent::CAtomicFixnum...
+# with GCC atomic operations
+#=> 1.270000   0.000000   1.270000 (  1.273004)
+#
+# Testing with Concurrent::CAtomicFixnum...
+# with pthread mutex
+#=> 1.980000   0.000000   1.980000 (  1.984493)
 
 # jruby 1.7.11 (1.9.3p392) 2014-02-24 86339bb on Java HotSpot(TM) 64-Bit Server VM 1.6.0_65-b14-462-11M4609 [darwin-x86_64]
-#=> 13.410000   7.510000  20.920000 ( 13.234000)
-#=> 4.130000   0.060000   4.190000 (  2.254000)
+#
+# Testing with Concurrent::MutexAtomicFixnum...
+#=> 12.060000   5.760000  17.820000 ( 11.731000)
+#
+# Testing with Concurrent::JavaAtomicFixnum...
+#=> 3.940000   0.070000   4.010000 (  2.118000)
