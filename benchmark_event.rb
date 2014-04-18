e95172ede3993109acbe0ba2862c9e8d9d9604d3
Jerry D'Antonio <stumpjumper@gmail.com>
2014-04-18 11:35:11 -0400

    Improved the benchmark test.

--- benchmark_event.rb
+++ benchmark_event.rb
@@ -20,7 +20,7 @@ def event_test(clazz, opts = {})
     end
 
     Thread.new do
-      tests.times{ event.set }
+      tests.times{ event.set; event.reset }
       latch.count_down
     end
 
@@ -30,10 +30,10 @@ def event_test(clazz, opts = {})
   print stats
 end
 
-event_test(Concurrent::Event, threads: 5, tests: 1_000_000)
+event_test(Concurrent::Event, threads: 10, tests: 1_000_000)
 
 if defined? Concurrent::CEvent
-  event_test(Concurrent::CEvent, threads: 5, tests: 1_000_000)
+  event_test(Concurrent::CEvent, threads: 10, tests: 1_000_000)
 end
 
 # About This Mac
@@ -43,12 +43,12 @@ end
 # ruby 2.1.1p76 (2014-02-24 revision 45161) [x86_64-darwin13.0]
 #
 # Testing with Concurrent::Event...
-#=> 39.020000  96.560000 135.580000 ( 88.018740)
+#=> 30.000000  73.960000 103.960000 ( 68.789397)
 #
 # Testing with Concurrent::CEvent...
-#=> 14.680000  36.790000  51.470000 ( 34.334118)
+#=> 18.550000  36.160000  54.710000 ( 39.766862)
 
 # jruby 1.7.11 (1.9.3p392) 2014-02-24 86339bb on Java HotSpot(TM) 64-Bit Server VM 1.6.0_65-b14-462-11M4609 [darwin-x86_64]
 #
 # Testing with Concurrent::Event...
-#=> 24.020000  11.990000  36.010000 ( 22.231000)
+#=> 24.880000  14.500000  39.380000 ( 25.637000)
