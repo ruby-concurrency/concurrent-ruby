bf5571985791776b8b68eec9dd67d94ac3ec0c3c
Petr Chalupa <git@pitr.ch>
2014-05-12 16:25:22 +0200

    whitespace

--- lib/concurrent/actor/simple_actor_ref.rb
+++ lib/concurrent/actor/simple_actor_ref.rb
@@ -9,21 +9,21 @@ module Concurrent
     include ActorRef
 
     def initialize(actor, opts = {})
-      @actor = actor
-      @mutex = Mutex.new
-      @one_by_one = OneByOne.new
-      @executor = OptionsParser::get_executor_from(opts)
-      @stop_event = Event.new
-      @reset_on_error = opts.fetch(:reset_on_error, true)
+      @actor           = actor
+      @mutex           = Mutex.new
+      @one_by_one      = OneByOne.new
+      @executor        = OptionsParser::get_executor_from(opts)
+      @stop_event      = Event.new
+      @reset_on_error  = opts.fetch(:reset_on_error, true)
       @exception_class = opts.fetch(:rescue_exception, false) ? Exception : StandardError
-      @args = opts.fetch(:args, []) if @reset_on_error
+      @args            = opts.fetch(:args, []) if @reset_on_error
 
       @actor.define_singleton_method(:shutdown, &method(:set_stop_event))
       @actor.on_start
     end
 
     def running?
-      ! @stop_event.set?
+      not @stop_event.set?
     end
 
     def shutdown?
