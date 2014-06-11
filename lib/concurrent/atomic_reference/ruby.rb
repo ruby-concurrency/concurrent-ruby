efbcb460d1d2d58dd58ea34c6fad086516c9da41
Jerry D'Antonio <stumpjumper@gmail.com>
2014-06-11 16:58:51 -0400

    Fixed CAtomic load error for Windows cross-compiled extensions.

--- lib/concurrent/atomic_reference/ruby.rb
+++ lib/concurrent/atomic_reference/ruby.rb
@@ -1,4 +1,10 @@
-require 'concurrent_cruby'
+begin
+  require 'concurrent_cruby'
+rescue LoadError
+  # may be a Windows cross-compiled native gem
+  require "#{RUBY_VERSION[0..2]}/concurrent_cruby"
+end
+
 require 'concurrent/atomic_reference/direct_update'
 require 'concurrent/atomic_reference/numeric_cas_wrapper'
 
