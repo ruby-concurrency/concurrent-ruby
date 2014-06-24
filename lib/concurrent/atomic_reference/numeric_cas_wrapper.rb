c067cabef72caa326ec9bb7653da2b07efbd1a5f
Jerry D'Antonio <stumpjumper@gmail.com>
2014-06-24 11:56:03 -0400

    Fixed Yardoc for Atomic

--- lib/concurrent/atomic_reference/numeric_cas_wrapper.rb
+++ lib/concurrent/atomic_reference/numeric_cas_wrapper.rb
@@ -4,20 +4,20 @@ module Concurrent
   module AtomicNumericCompareAndSetWrapper
 
     # @!macro atomic_reference_method_compare_and_set
-    def compare_and_set(expected, new)
-      if expected.kind_of? Numeric
+    def compare_and_set(old_value, new_value)
+      if old_value.kind_of? Numeric
         while true
           old = get
 
           return false unless old.kind_of? Numeric
 
-          return false unless old == expected
+          return false unless old == old_value
 
-          result = _compare_and_set(old, new)
+          result = _compare_and_set(old, new_value)
           return result if result
         end
       else
-        _compare_and_set(expected, new)
+        _compare_and_set(old_value, new_value)
       end
     end
     alias_method :compare_and_swap, :compare_and_set
