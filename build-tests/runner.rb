45e221420df78fca1dee1e844aa06eb53e26da77
Jerry D'Antonio <stumpjumper@gmail.com>
2014-12-14 21:08:05 -0500

    Improved build test runner.

--- build-tests/runner.rb
+++ build-tests/runner.rb
@@ -26,6 +26,7 @@ EXT_PLATFORMS = {
 
 TEST_PATH = File.dirname(__FILE__)
 PKG_PATH = File.join(File.dirname(__FILE__), '..', 'pkg')
+TEST_FILES = Dir["#{TEST_PATH}/*_spec.rb"]
 
 RSPEC = "rspec --default-path #{TEST_PATH} -fd --color --seed 0"
 
@@ -36,7 +37,6 @@ gem uninstall -q -a -I ref
 CMD
 
 SUITE_BREAK = "######################################################################\n"
-GEM_BREAK   = "----------------------------------------------------------------------\n"
 
 def platform_specific_extensions?(platform = RUBY_PLATFORM)
   EXT_PLATFORMS.keys.include?(platform) &&
@@ -61,7 +61,8 @@ def install_java_gem_command
   "gem install #{PKG_PATH}/concurrent-ruby-#{Concurrent::VERSION}-java.gem"
 end
 
-def run_tests_cmd(file, ext, platform = '')
+def run_test_suite(files, ext, platform = '')
+
   test_platform = if ext
                     'EXT'
                   elsif jruby?(platform)
@@ -75,31 +76,26 @@ def run_tests_cmd(file, ext, platform = '')
         else
           install_gems_command(ext, platform)
         end
+  ok = system(cmd)
 
-  cmd << "\n"
-  cmd << "TEST_PLATFORM='#{test_platform}' #{RSPEC} #{file}"
-  cmd << "\n"
-  cmd << UNINSTALL_GEMS_COMMAND
-  cmd
+  files.each do |file|
+    cmd = "TEST_PLATFORM='#{test_platform}' #{RSPEC} #{file}"
+    ok = system(cmd)
+  end
+
+  ok = system(UNINSTALL_GEMS_COMMAND)
 end
 
-TESTS = Dir["#{TEST_PATH}/*_spec.rb"]
-ok = system(UNINSTALL_GEMS_COMMAND)
-
-TESTS.each do |file|
+puts SUITE_BREAK
+run_test_suite(TEST_FILES, false)
+if jruby?
   puts SUITE_BREAK
-  puts "Running #{file}"
-  puts GEM_BREAK
-  ok = system(run_tests_cmd(file, false))
-  if jruby?
-    puts GEM_BREAK
-    ok = system(run_tests_cmd(file, false, 'jruby'))
-  elsif mri?
-    puts GEM_BREAK
-    ok = system(run_tests_cmd(file, true))
-    if platform_specific_extensions?(RUBY_PLATFORM)
-      puts GEM_BREAK
-      ok = system(run_tests_cmd(file, true, RUBY_PLATFORM))
-    end
+  run_test_suite(TEST_FILES, false, 'jruby')
+elsif mri?
+  puts SUITE_BREAK
+  run_test_suite(TEST_FILES, true)
+  if platform_specific_extensions?(RUBY_PLATFORM)
+    puts SUITE_BREAK
+    run_test_suite(TEST_FILES, true, RUBY_PLATFORM)
   end
 end
