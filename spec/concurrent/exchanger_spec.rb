19845f612df0787c1db5fe8b663900321d58e6cf
Jerry D'Antonio <stumpjumper@gmail.com>
2015-03-06 16:54:35 -0500

    Better determinism in Exchanger tests.

--- spec/concurrent/exchanger_spec.rb
+++ spec/concurrent/exchanger_spec.rb
@@ -1,3 +1,5 @@
+require 'hitimes'
+
 module Concurrent
 
   describe Exchanger do
@@ -6,39 +8,68 @@ module Concurrent
     let!(:exchanger) { subject } # let is not thread safe, let! creates the object before ensuring uniqueness
 
     describe 'exchange' do
+
       context 'without timeout' do
+
         it 'should block' do
-          t = Thread.new { exchanger.exchange(1) }
-          sleep(0.1)
+          latch = Concurrent::CountDownLatch.new
+
+          t = Thread.new do
+            latch.count_down
+            exchanger.exchange(1)
+          end
+
+          latch.wait(1)
           expect(t.status).to eq 'sleep'
         end
 
         it 'should receive the other value' do
+          latch = Concurrent::CountDownLatch.new(2)
           first_value = nil
           second_value = nil
 
-          Thread.new { first_value = exchanger.exchange(2) }
-          Thread.new { second_value = exchanger.exchange(4) }
+          Thread.new do
+            first_value = exchanger.exchange(2)
+            latch.count_down
+          end
+          Thread.new do
+            second_value = exchanger.exchange(4)
+            latch.count_down
+          end
 
-          sleep(0.1)
-
+          latch.wait(1)
           expect(first_value).to eq 4
           expect(second_value).to eq 2
         end
 
         it 'can be reused' do
+          latch_1 = Concurrent::CountDownLatch.new(2)
+          latch_2 = Concurrent::CountDownLatch.new(2)
+
           first_value = nil
           second_value = nil
 
-          Thread.new { first_value = exchanger.exchange(1) }
-          Thread.new { second_value = exchanger.exchange(0) }
+          Thread.new do
+            first_value = exchanger.exchange(1)
+            latch_1.count_down
+          end
+          Thread.new do
+            second_value = exchanger.exchange(0)
+            latch_1.count_down
+          end
 
-          sleep(0.1)
+          latch_1.wait(1)
 
-          Thread.new { first_value = exchanger.exchange(10) }
-          Thread.new { second_value = exchanger.exchange(12) }
+          Thread.new do
+            first_value = exchanger.exchange(10)
+            latch_2.count_down
+          end
+          Thread.new do
+            second_value = exchanger.exchange(12)
+            latch_2.count_down
+          end
 
-          sleep(0.1)
+          latch_2.wait(1)
 
           expect(first_value).to eq 12
           expect(second_value).to eq 10
@@ -48,9 +79,10 @@ module Concurrent
       context 'with timeout' do
 
         it 'should block until timeout' do
-          start = Time.now.to_f
-          expect(exchanger.exchange(2, 0.1)).to be_nil
-          expect(Time.now.to_f - start).to be_within(0.05).of(0.1)
+          duration = Hitimes::Interval.measure do
+            exchanger.exchange(2, 0.1)
+          end
+          expect(duration).to be_within(0.05).of(0.1)
         end
       end
     end
