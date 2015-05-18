0f88dc110c3a1450d527859fdf1683f2fc9a41c8
Petr Chalupa <git@pitr.ch>
2015-05-18 21:21:13 +0200

    Usage examples of new Futures

--- examples/benchmark_new_futures.rb
+++ examples/benchmark_new_futures.rb
@@ -7,28 +7,35 @@ time   = 10 * scale
 warmup = 2 * scale
 warmup *= 10 if Concurrent.on_jruby?
 
+Benchmark.ips(time, warmup) do |x|
+  x.report('graph-old') do
+    head    = Concurrent::Promise.execute { 1 }
+    branch1 = head.then(&:succ)
+    branch2 = head.then(&:succ).then(&:succ)
+    branch3 = head.then(&:succ).then(&:succ).then(&:succ)
+    Concurrent::Promise.zip(branch1, branch2, branch3).then { |(a, b, c)| a + b + c }.value!
+  end
+  x.report('graph-new') do
+    head    = Concurrent.future { 1 }
+    branch1 = head.then(&:succ)
+    branch2 = head.then(&:succ).then(&:succ)
+    branch3 = head.then(&:succ).then(&:succ).then(&:succ)
+    Concurrent.zip(branch1, branch2, branch3).then { |(a, b, c)| a + b + c }.value!
+  end
+  x.compare!
+end
+
+Benchmark.ips(time, warmup) do |x|
+  x.report('status-old') { f = Concurrent::Promise.execute { nil }; 10.times { f.complete? } }
+  x.report('status-new') { f = Concurrent.future { nil }; 10.times { f.completed? } }
+  x.compare!
+end
 
 Benchmark.ips(time, warmup) do |x|
   of = Concurrent::Promise.execute { 1 }
   nf = Concurrent.future { 1 }
   x.report('value-old') { of.value! }
   x.report('value-new') { nf.value! }
-  x.compare!
-end
-
-Benchmark.ips(time, warmup) do |x|
-  x.report('graph-old') do
-    head    = Concurrent::Promise.execute { 1 }
-    branch1 = head.then(&:succ)
-    branch2 = head.then(&:succ).then(&:succ)
-    Concurrent::Promise.zip(branch1, branch2).then { |(a, b)| a + b }.value!
-  end
-  x.report('graph-new') do
-    head    = Concurrent.future { 1 }
-    branch1 = head.then(&:succ)
-    branch2 = head.then(&:succ).then(&:succ)
-    (branch1 + branch2).then { |(a, b)| a + b }.value!
-  end
   x.compare!
 end
 
