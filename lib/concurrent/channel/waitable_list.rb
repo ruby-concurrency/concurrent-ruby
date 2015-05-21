d8459820305ce0c297f584c4e77cfcc1d0ec63ef
Jerry D'Antonio <stumpjumper@gmail.com>
2015-05-21 16:40:13 -0600

    Moved all channel classes under the Channel module.

--- lib/concurrent/channel/waitable_list.rb
+++ lib/concurrent/channel/waitable_list.rb
@@ -1,38 +1,40 @@
 require 'concurrent/synchronization_object'
 
 module Concurrent
-  class WaitableList < SynchronizationObject
+  module Channel
+    class WaitableList < SynchronizationObject
 
-    def size
-      synchronize { @list.size }
-    end
-
-    def empty?
-      synchronize { @list.empty? }
-    end
-
-    def put(value)
-      synchronize do
-        @list << value
-        ns_signal
+      def size
+        synchronize { @list.size }
       end
-    end
 
-    def delete(value)
-      synchronize { @list.delete(value) }
-    end
-
-    def take
-      synchronize do
-        ns_wait_until { !@list.empty? }
-        @list.shift
+      def empty?
+        synchronize { @list.empty? }
       end
-    end
 
-    protected
+      def put(value)
+        synchronize do
+          @list << value
+          ns_signal
+        end
+      end
 
-    def ns_initialize
-      @list = []
+      def delete(value)
+        synchronize { @list.delete(value) }
+      end
+
+      def take
+        synchronize do
+          ns_wait_until { !@list.empty? }
+          @list.shift
+        end
+      end
+
+      protected
+
+      def ns_initialize
+        @list = []
+      end
     end
   end
 end
