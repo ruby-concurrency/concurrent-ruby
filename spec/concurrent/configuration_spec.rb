c8ee4b5e94079069f548d0fa3867bf9aa74e9502
Jerry D'Antonio <stumpjumper@gmail.com>
2015-06-02 10:56:20 -0400

    Fixed broken tests.

--- spec/concurrent/configuration_spec.rb
+++ spec/concurrent/configuration_spec.rb
@@ -28,9 +28,10 @@ module Concurrent
       end
 
       specify '#terminate_pools! acts on all executors with auto_terminate: true' do
-        expect(Concurrent.global_fast_executor).to receive(:kill).once.with(no_args).and_call_original
-        expect(Concurrent.global_io_executor).to receive(:kill).once.with(no_args).and_call_original
-        expect(Concurrent.global_timer_set).to receive(:kill).once.with(no_args).and_call_original
+        # The 'at_least(:once)' clauses account for global config reset
+        expect(Concurrent.global_fast_executor).to receive(:kill).at_least(:once).with(no_args).and_call_original
+        expect(Concurrent.global_io_executor).to receive(:kill).at_least(:once).with(no_args).and_call_original
+        expect(Concurrent.global_timer_set).to receive(:kill).at_least(:once).with(no_args).and_call_original
         Concurrent.terminate_pools!
       end
     end
