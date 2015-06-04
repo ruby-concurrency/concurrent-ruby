12b1ab12a0959c2d5561f128279adad62522a410
Jerry D'Antonio <stumpjumper@gmail.com>
2015-06-04 03:27:12 -0400

    Fixed build tests.

--- build-tests/atomic_reference_builds_spec.rb
+++ build-tests/atomic_reference_builds_spec.rb
@@ -23,62 +23,62 @@ def atomic_reference_test(clazz, opts = {})
   stats
 end
 
-describe Concurrent::Atomic do
+describe Concurrent::AtomicReference do
 
   let!(:threads) { 10 }
   let!(:tests) { 1000 }
 
-  describe Concurrent::MutexAtomic do
+  describe Concurrent::MutexAtomicReference do
 
     specify 'is defined' do
-      expect(defined?(Concurrent::MutexAtomic)).to be_truthy
+      expect(defined?(Concurrent::MutexAtomicReference)).to be_truthy
     end
 
     specify 'runs the benchmarks' do
-      stats = atomic_reference_test('MutexAtomic', threads: threads, tests: tests)
+      stats = atomic_reference_test('MutexAtomicReference', threads: threads, tests: tests)
       expect(stats).to be_benchmark_results
     end
   end
 
   if jruby? && 'JRUBY' == ENV['TEST_PLATFORM'].strip
 
-    describe Concurrent::JavaAtomic do
+    describe Concurrent::JavaAtomicReference do
 
-      specify 'Concurrent::JavaAtomic is defined' do
-        expect(defined?(Concurrent::JavaAtomic)).to be_truthy
+      specify 'Concurrent::JavaAtomicReference is defined' do
+        expect(defined?(Concurrent::JavaAtomicReference)).to be_truthy
       end
 
       specify 'runs the benchmarks' do
-        stats = atomic_reference_test('JavaAtomic', threads: threads, tests: tests)
+        stats = atomic_reference_test('JavaAtomicReference', threads: threads, tests: tests)
         expect(stats).to be_benchmark_results
       end
     end
 
   else
 
-    specify 'Concurrent::JavaAtomic is not defined' do
-      expect(defined?(Concurrent::JavaAtomic)).to be_falsey
+    specify 'Concurrent::JavaAtomicReference is not defined' do
+      expect(defined?(Concurrent::JavaAtomicReference)).to be_falsey
     end
   end
 
   if 'EXT' == ENV['TEST_PLATFORM'].strip
 
-    describe Concurrent::CAtomic do
+    describe Concurrent::CAtomicReference do
 
-      specify 'Concurrent::CAtomic is defined' do
-        expect(defined?(Concurrent::CAtomic)).to be_truthy
+      specify 'Concurrent::CAtomicReference is defined' do
+        expect(defined?(Concurrent::CAtomicReference)).to be_truthy
       end
 
       specify 'runs the benchmarks' do
-        stats = atomic_reference_test('CAtomic', threads: threads, tests: tests)
+        stats = atomic_reference_test('CAtomicReference', threads: threads, tests: tests)
         expect(stats).to be_benchmark_results
       end
     end
 
   else
 
-    specify 'Concurrent::CAtomic is not defined' do
-      expect(defined?(Concurrent::CAtomic)).to be_falsey
+    specify 'Concurrent::CAtomicReference is not defined' do
+      expect(defined?(Concurrent::CAtomicReference)).to be_falsey
     end
   end
 end
