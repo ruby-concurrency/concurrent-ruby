bc71df1fa5952c8d6d549871210d81b9619026f8
Petr Chalupa <git@pitr.ch>
2015-06-16 21:25:17 +0200

    Add common ancestor to Concurrent errors

--- lib/concurrent/errors.rb
+++ lib/concurrent/errors.rb
@@ -1,40 +1,42 @@
 module Concurrent
 
+  Error = Class.new(StandardError)
+
   # Raised when errors occur during configuration.
-  ConfigurationError = Class.new(StandardError)
+  ConfigurationError = Class.new(Error)
 
   # Raised when an asynchronous operation is cancelled before execution.
-  CancelledOperationError = Class.new(StandardError)
+  CancelledOperationError = Class.new(Error)
 
   # Raised when a lifecycle method (such as `stop`) is called in an improper
   # sequence or when the object is in an inappropriate state.
-  LifecycleError = Class.new(StandardError)
+  LifecycleError = Class.new(Error)
 
   # Raised when an attempt is made to violate an immutability guarantee.
-  ImmutabilityError = Class.new(StandardError)
+  ImmutabilityError = Class.new(Error)
 
   # Raised when an object's methods are called when it has not been
   # properly initialized.
-  InitializationError = Class.new(StandardError)
+  InitializationError = Class.new(Error)
 
   # Raised when an object with a start/stop lifecycle has been started an
   # excessive number of times. Often used in conjunction with a restart
   # policy or strategy.
-  MaxRestartFrequencyError = Class.new(StandardError)
+  MaxRestartFrequencyError = Class.new(Error)
 
   # Raised when an attempt is made to modify an immutable object
   # (such as an `IVar`) after its final state has been set.
-  MultipleAssignmentError = Class.new(StandardError)
+  MultipleAssignmentError = Class.new(Error)
 
   # Raised by an `Executor` when it is unable to process a given task,
   # possibly because of a reject policy or other internal error.
-  RejectedExecutionError = Class.new(StandardError)
+  RejectedExecutionError = Class.new(Error)
 
   # Raised when any finite resource, such as a lock counter, exceeds its
   # maximum limit/threshold.
-  ResourceLimitError = Class.new(StandardError)
+  ResourceLimitError = Class.new(Error)
 
   # Raised when an operation times out.
-  TimeoutError = Class.new(StandardError)
+  TimeoutError = Class.new(Error)
 
 end
