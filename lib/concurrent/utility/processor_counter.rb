494652eb51e1db13872f2fe1ddff7878e78bd6ab
Chris Seaton <chris@chrisseaton.com>
2016-01-19 21:08:54 +0000

    Use Truffle's primitive for available processors.

--- lib/concurrent/utility/processor_counter.rb
+++ lib/concurrent/utility/processor_counter.rb
@@ -75,6 +75,8 @@ module Concurrent
       def compute_processor_count
         if Concurrent.on_jruby?
           java.lang.Runtime.getRuntime.availableProcessors
+        elsif Concurrent.on_truffle?
+          Truffle::Primitive.logical_processors
         else
           os_name = RbConfig::CONFIG["target_os"]
           if os_name =~ /mingw|mswin/
