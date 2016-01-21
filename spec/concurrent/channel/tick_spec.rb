22135df2aa74369ee6233468899311e1f22c1cfb
Chris Seaton <chris@chrisseaton.com>
2016-01-21 22:27:40 +0000

    Add :truffle_bug as a tag on some specs that don't work on JRuby+Truffle.

--- spec/concurrent/channel/tick_spec.rb
+++ spec/concurrent/channel/tick_spec.rb
@@ -26,7 +26,7 @@ module Concurrent
         expect(subject.utc.to_f).to eq subject.epoch
       end
 
-      specify do
+      specify '#to_s formats as a time', :truffle_bug => true do
         expect(subject.to_s).to match /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6} \+\d{4} UTC/
       end
 
