88166ef572cf9a9a08d569845c2f9aabaa4b60b9
Ivo Anjo <ivo.anjo@ist.utl.pt>
2016-07-23 08:25:07 -0700

    Minor cleanups and simplifications in thread_local_var_spec

--- spec/concurrent/atomic/thread_local_var_spec.rb
+++ spec/concurrent/atomic/thread_local_var_spec.rb
@@ -6,22 +6,20 @@ module Concurrent
 
   describe ThreadLocalVar do
 
-    subject { ThreadLocalVar.new }
-
     context '#initialize' do
 
       it 'can set an initial value' do
-        v = ThreadLocalVar.new(14)
+        v = described_class.new(14)
         expect(v.value).to eq 14
       end
 
       it 'sets nil as a default initial value' do
-        v = ThreadLocalVar.new
+        v = described_class.new
         expect(v.value).to be_nil
       end
 
       it 'sets the same initial value for all threads' do
-        v  = ThreadLocalVar.new(14)
+        v  = described_class.new(14)
         t1 = Thread.new { v.value }
         t2 = Thread.new { v.value }
         expect(t1.value).to eq 14
@@ -30,51 +28,47 @@ module Concurrent
 
       if Concurrent.on_jruby?
         it 'extends JavaThreadLocalVar' do
-          expect(subject.class.ancestors).to include(Concurrent::JavaThreadLocalVar)
+          expect(described_class.ancestors).to include(Concurrent::JavaThreadLocalVar)
         end
       else
         it 'extends RubyThreadLocalVar' do
-          expect(subject.class.ancestors).to include(Concurrent::RubyThreadLocalVar)
+          expect(described_class.ancestors).to include(Concurrent::RubyThreadLocalVar)
         end
       end
     end
 
     context '#value' do
+      let(:v) { described_class.new(14) }
 
       it 'returns the current value' do
-        v = ThreadLocalVar.new(14)
         expect(v.value).to eq 14
       end
 
       it 'returns the value after modification' do
-        v = ThreadLocalVar.new(14)
         v.value = 2
         expect(v.value).to eq 2
       end
     end
 
     context '#value=' do
+      let(:v) { described_class.new(14) }
 
       it 'sets a new value' do
-        v = ThreadLocalVar.new(14)
         v.value = 2
         expect(v.value).to eq 2
       end
 
       it 'returns the new value' do
-        v = ThreadLocalVar.new(14)
         expect(v.value = 2).to eq 2
       end
 
       it 'does not modify the initial value for other threads' do
-        v = ThreadLocalVar.new(14)
         v.value = 2
         t = Thread.new { v.value }
         expect(t.value).to eq 14
       end
 
       it 'does not modify the value for other threads' do
-        v = ThreadLocalVar.new(14)
         v.value = 2
 
         b1 = CountDownLatch.new(2)
