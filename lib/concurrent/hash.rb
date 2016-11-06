6f9d974d1982e174353b5f7ff44d85a1c5d20ce0
Petr Chalupa <git@pitr.ch>
2016-11-06 17:57:21 +0100

    Clarify Concurrent:Hash#each documentation

--- lib/concurrent/hash.rb
+++ lib/concurrent/hash.rb
@@ -8,7 +8,8 @@ module Concurrent
     #
     #   A thread-safe subclass of Hash. This version locks against the object
     #   itself for every method call, ensuring only one thread can be reading
-    #   or writing at a time. This includes iteration methods like `#each`.
+    #   or writing at a time. This includes iteration methods like `#each`,
+    #   which takes the lock repeatedly when reading an item.
     #
     #   @see http://ruby-doc.org/core-2.2.0/Hash.html Ruby standard library `Hash`
     class Hash < ::Hash;
