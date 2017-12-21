0559cbacce1dce2fcde2dc7c7b9f9f166abe1549
David Harsha <dharsha@swiftype.com>
2017-12-21 11:08:15 -0800

    Add `executor` and `execute` options to zip docs

--- lib/concurrent/promise.rb
+++ lib/concurrent/promise.rb
@@ -383,7 +383,14 @@ module Concurrent
     # Builds a promise that produces the result of promises in an Array
     # and fails if any of them fails.
     #
-    # @param [Array<Promise>] promises
+    # @overload zip(*promises)
+    #   @param [Array<Promise>] promises
+    #
+    # @overload zip(*promises, opts)
+    #   @param [Array<Promise>] promises
+    #   @param [Hash] opts the configuration options
+    #   @option opts [Executor] :executor (ImmediateExecutor.new) when set use the given `Executor` instance.
+    #   @option opts [Boolean] :execute (true) execute promise before returning
     #
     # @return [Promise<Array>]
     def self.zip(*promises)
@@ -407,7 +414,14 @@ module Concurrent
     # Builds a promise that produces the result of self and others in an Array
     # and fails if any of them fails.
     #
-    # @param [Array<Promise>] others
+    # @overload zip(*promises)
+    #   @param [Array<Promise>] others
+    #
+    # @overload zip(*promises, opts)
+    #   @param [Array<Promise>] others
+    #   @param [Hash] opts the configuration options
+    #   @option opts [Executor] :executor (ImmediateExecutor.new) when set use the given `Executor` instance.
+    #   @option opts [Boolean] :execute (true) execute promise before returning
     #
     # @return [Promise<Array>]
     def zip(*others)
