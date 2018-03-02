82cae34cd74f2866034899d26a5362844528e6be
Petr Chalupa <git@pitr.ch>
2018-03-02 17:57:51 +0100

    Do not reset global threads

--- spec/concurrent/configuration_spec.rb
+++ spec/concurrent/configuration_spec.rb
@@ -2,14 +2,6 @@ module Concurrent
 
    RSpec.describe 'configuration', notravis: true do
 
-    before(:all) do
-      reset_gem_configuration
-    end
-
-    after(:each) do
-      reset_gem_configuration
-    end
-
     context 'global executors' do
 
       it 'creates a global timer set' do
@@ -29,9 +21,9 @@ module Concurrent
 
       specify 'Concurrent::AtExit.run acts on all executors with auto_terminate: true' do
         # The 'at_least(:once)' clauses account for global config reset
-        expect(Concurrent.global_fast_executor).to receive(:kill).at_least(:once).with(no_args).and_call_original
-        expect(Concurrent.global_io_executor).to receive(:kill).at_least(:once).with(no_args).and_call_original
-        expect(Concurrent.global_timer_set).to receive(:kill).at_least(:once).with(no_args).and_call_original
+        expect(Concurrent.global_fast_executor).to receive(:kill).at_least(:once).with(no_args)
+        expect(Concurrent.global_io_executor).to receive(:kill).at_least(:once).with(no_args)
+        expect(Concurrent.global_timer_set).to receive(:kill).at_least(:once).with(no_args)
         Concurrent::AtExit.run
       end
     end
