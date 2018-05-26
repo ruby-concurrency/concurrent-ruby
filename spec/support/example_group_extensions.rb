cb8903a229d99a1897cf518c5d3755cd9b7961a8
Petr Chalupa <git@pitr.ch>
2018-05-26 23:00:50 +0200

    Use rspec configuration properly

--- spec/support/example_group_extensions.rb
+++ spec/support/example_group_extensions.rb
@@ -28,7 +28,7 @@ module Concurrent
 
     def in_thread(*args, &block)
       @created_threads ||= Queue.new
-      new_thread = Thread.new(*args) do |*args, &b|
+      new_thread       = Thread.new(*args) do |*args, &b|
         Thread.abort_on_exception = true
         block.call *args, &b
       end
@@ -46,22 +46,6 @@ module Concurrent
         joined_thread = t.join(timeout * threads.size)
         expect(joined_thread).not_to eq nil
       end
-    end
-  end
-end
-
-class RSpec::Core::ExampleGroup
-  include Concurrent::TestHelpers
-  extend Concurrent::TestHelpers
-
-  before :each do
-    expect(@created_threads.nil? || @created_threads.empty?).to be_truthy
-  end
-
-  after :each do
-    while (thread = (@created_threads.pop(true) rescue nil))
-      thread.kill
-      expect(thread.join(0.25)).not_to be_nil
     end
   end
 end
