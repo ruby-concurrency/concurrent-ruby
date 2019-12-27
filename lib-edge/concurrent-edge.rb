d794b95cc785bc911eeced76055999c966f59b1a
Petr Chalupa <git@pitr.ch>
2019-12-27 16:33:20 +0100

    Load WrappingExecutor only on newer Ruby

--- lib-edge/concurrent-edge.rb
+++ lib-edge/concurrent-edge.rb
@@ -6,7 +6,7 @@ require 'concurrent/actor'
 require 'concurrent/agent'
 require 'concurrent/channel'
 require 'concurrent/lazy_register'
-require 'concurrent/executor/wrapping_executor'
+require 'concurrent/executor/wrapping_executor' if Concurrent.ruby_version :>=, 2, 1, 0
 
 require 'concurrent/edge/lock_free_linked_set'
 require 'concurrent/edge/lock_free_queue'
