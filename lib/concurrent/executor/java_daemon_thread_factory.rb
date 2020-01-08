94fd5d84f9fe5850a9d242696d218e9d8d0f52bb
Farid Zakaria <farid.m.zakaria@gmail.com>
2020-01-08 10:06:26 -0800

    Introduce DaemonThreadFactory
    Introduce a ThreadFactory that will set `Thread#setDaemon` according
    to the _auto_terminate_ value.
    
    This makes the need for the `AtExit` functionality uneeded since all
    threads can now be started daemonized.
    
    Furthermore, the gross **hack** of `Configuration#disable_at_exit_handlers!`
    is especially not needed since this allows the JVM to properly terminate
    even with minitest.
    
    You can test this with:
    ```
    ruby test/concurrent/executor/test_cached_thread_pool.rb
    ```
    
    fixes #839
    fixes #817

---
+++ lib/concurrent/executor/java_daemon_thread_factory.rb
@@ -0,0 +1,18 @@
+module Concurrent
+
+  class DaemonThreadFactory
+    include java.util.concurrent.ThreadFactory
+
+    def initialize(daemonize = true)
+      @daemonize = daemonize
+    end
+
+    def newThread(runnable)
+      thread = java.util.concurrent.Executors.defaultThreadFactory().newThread(runnable)
+      thread.setDaemon(@daemonize)
+      return thread
+    end
+
+  end
+
+end
