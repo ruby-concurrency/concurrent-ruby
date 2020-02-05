f2de94a1f55f1d85dc238bb3d445857eba26bc64
Petr Chalupa <git@pitr.ch>
2020-02-05 20:50:47 +0100

    Allow the child process to be already gone

--- spec/concurrent/executor/cached_thread_pool_spec.rb
+++ spec/concurrent/executor/cached_thread_pool_spec.rb
@@ -212,6 +212,8 @@ module Concurrent
               pid       = spawn RbConfig.ruby, test_file
               Process.waitpid pid
               expect($?.success?).to eq true
+            rescue Errno::ECHILD
+              # child already gone
             rescue Timeout::Error => e
               Process.kill :KILL, pid
               raise e
