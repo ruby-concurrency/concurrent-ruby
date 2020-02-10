99dc326a0b9c7954bf40ddd574c4d501def1c343
Petr Chalupa <git@pitr.ch>
2020-02-10 21:18:36 +0100

    Fix placement and add documentation

--- lib/concurrent-ruby/concurrent/atomic/constant_thread_local_var.rb
+++ lib/concurrent-ruby/concurrent/atomic/constant_thread_local_var.rb
@@ -1,17 +1,26 @@
 require 'concurrent/atomic/abstract_thread_local_var'
 
 module Concurrent
+  # Has the same interface as {ThreadLocalVar} but the value can never be changed.
+  # The value is always the default provided to the constructor.
   class ConstantThreadLocalVar < AbstractThreadLocalVar
+
+    # @note the value is always equal to default value
+    # @!macro thread_local_var_method_get
     def value
       default
     end
 
+    # @!macro thread_local_var_method_set
+    # @raise ArgumentError if the value is not equal to the default
     def value=(value)
       if value != default
         raise ArgumentError, "Constant thread local vars may not be altered"
       end
     end
 
+    # @!macro thread_local_var_method_bind
+    # @raise ArgumentError if the value is not equal to the default
     def bind(value)
       self.value = value
       if block_given?
@@ -20,6 +29,7 @@ module Concurrent
     end
 
     protected
+
     def allocate_storage
       # nothing to do
     end
