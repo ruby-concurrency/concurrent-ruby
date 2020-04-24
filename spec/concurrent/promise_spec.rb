a14fdc60ef96fab27e931268b9ec2d72e1ad3c6c
John Pelly <jpelly@gmail.com>
2020-04-24 13:49:47 -0400

    bug fix (& spec): After parent has completed (via #wait, #value, etc), creating a child promise (via #then, #on_success, etc) and calling child_promise.execute.state return :unscheduled. It should return :pending

--- spec/concurrent/promise_spec.rb
+++ spec/concurrent/promise_spec.rb
@@ -195,6 +195,14 @@ module Concurrent
             end_latch.count_down
           end
         end
+
+        context "when called on child after parent completes" do
+          let(:parent_promise) { Concurrent::Promise.execute { 1 + 1 }.tap { |p| p.wait } }
+          it 'sets state to :pending immediately' do
+            child_promise = parent_promise.then { |two| two + 2 }.execute
+            expect(child_promise.state).to eq(:pending)
+          end
+        end
       end
     end
 
