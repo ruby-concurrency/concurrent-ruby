fa5f28a6bb87d0cf902f9e243764c256e7364d7c
Benoit Daloze <eregontp@gmail.com>
2023-01-11 16:22:02 +0100

    The java extension is non-optional

--- lib/concurrent-ruby/concurrent/synchronization/jruby_lockable_object.rb
+++ lib/concurrent-ruby/concurrent/synchronization/jruby_lockable_object.rb
@@ -1,7 +1,9 @@
+require 'concurrent/utility/native_extension_loader' # load native parts first
+
 module Concurrent
   module Synchronization
 
-    if Concurrent.on_jruby? && Concurrent.java_extensions_loaded?
+    if Concurrent.on_jruby?
 
       # @!visibility private
       # @!macro internal_implementation_note
