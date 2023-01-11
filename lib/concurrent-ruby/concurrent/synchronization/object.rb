1bd4a48201e1cc4bc6b54a649c48a7012210bf13
Benoit Daloze <eregontp@gmail.com>
2023-01-11 16:03:55 +0100

    Simplify dependencies in concurrent/synchronization/volatile

--- lib/concurrent-ruby/concurrent/synchronization/object.rb
+++ lib/concurrent-ruby/concurrent/synchronization/object.rb
@@ -1,3 +1,5 @@
+require 'concurrent/utility/native_extension_loader' # load native parts first
+
 require 'concurrent/synchronization/safe_initialization'
 require 'concurrent/synchronization/volatile'
 require 'concurrent/atomic/atomic_reference'
--- lib/concurrent-ruby/concurrent/synchronization/volatile.rb
+++ lib/concurrent-ruby/concurrent/synchronization/volatile.rb
@@ -1,3 +1,6 @@
+require 'concurrent/utility/native_extension_loader' # load native parts first
+require 'concurrent/synchronization/full_memory_barrier'
+
 module Concurrent
   module Synchronization
 
@@ -20,22 +23,77 @@ module Concurrent
     #  foo.bar = 2
     #  => 2
 
-    Volatile = case
-               when Concurrent.on_cruby?
-                 MriAttrVolatile
-               when Concurrent.on_jruby?
-                 JRubyAttrVolatile
-               when Concurrent.on_truffleruby?
-                 TruffleRubyAttrVolatile
-               else
-                 warn 'Possibly unsupported Ruby implementation'
-                 MriAttrVolatile
-               end
-
-    Volatile.class_exec do
-      def full_memory_barrier
-        Concurrent::Synchronization.full_memory_barrier
+    module Volatile
+      def self.included(base)
+        base.extend(ClassMethods)
       end
+
+      def full_memory_barrier
+        Synchronization.full_memory_barrier
+      end
+
+      module ClassMethods
+        if Concurrent.on_cruby?
+          def attr_volatile(*names)
+            names.each do |name|
+              ivar = :"@volatile_#{name}"
+              class_eval <<-RUBY, __FILE__, __LINE__ + 1
+                def #{name}
+                  #{ivar}
+                end
+
+                def #{name}=(value)
+                  #{ivar} = value
+                end
+              RUBY
+            end
+            names.map { |n| [n, :"#{n}="] }.flatten
+          end
+
+        elsif Concurrent.on_jruby?
+          def attr_volatile(*names)
+            names.each do |name|
+              ivar = :"@volatile_#{name}"
+
+              class_eval <<-RUBY, __FILE__, __LINE__ + 1
+                def #{name}
+                  ::Concurrent::Synchronization::JRubyAttrVolatile.instance_variable_get_volatile(self, :#{ivar})
+                end
+
+                def #{name}=(value)
+                  ::Concurrent::Synchronization::JRubyAttrVolatile.instance_variable_set_volatile(self, :#{ivar}, value)
+                end
+              RUBY
+
+            end
+            names.map { |n| [n, :"#{n}="] }.flatten
+          end
+
+        else
+          warn 'Possibly unsupported Ruby implementation' unless Concurrent.on_truffleruby?
+
+          def attr_volatile(*names)
+            names.each do |name|
+              ivar = :"@volatile_#{name}"
+
+              class_eval <<-RUBY, __FILE__, __LINE__ + 1
+                def #{name}
+                  ::Concurrent::Synchronization.full_memory_barrier
+                  #{ivar}
+                end
+
+                def #{name}=(value)
+                  #{ivar} = value
+                  ::Concurrent::Synchronization.full_memory_barrier
+                end
+              RUBY
+            end
+
+            names.map { |n| [n, :"#{n}="] }.flatten
+          end
+        end
+      end
+
     end
   end
 end
