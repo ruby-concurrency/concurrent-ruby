970dd0ff71789b33ce3f80005c68cc98a0c58d4b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-01-12 14:42:12 +1300

    Revert back to shared implementation of "locals" storage array.

--- lib/concurrent-ruby/concurrent/atomic/fiber_local_var.rb
+++ lib/concurrent-ruby/concurrent/atomic/fiber_local_var.rb
@@ -1,7 +1,40 @@
 require 'concurrent/constants'
+require_relative 'locals'
 
 module Concurrent
   class FiberLocalVar
+    LOCALS = FiberLocals.new(:concurrent_fiber_local_var)
+
+    # @!macro fiber_local_var_method_initialize
+    #
+    #   Creates a fiber local variable.
+    #
+    #   @param [Object] default the default value when otherwise unset
+    #   @param [Proc] default_block Optional block that gets called to obtain the
+    #     default value for each fiber
+
+    # @!macro fiber_local_var_method_get
+    #
+    #   Returns the value in the current fiber's copy of this fiber-local variable.
+    #
+    #   @return [Object] the current value
+
+    # @!macro fiber_local_var_method_set
+    #
+    #   Sets the current fiber's copy of this fiber-local variable to the specified value.
+    #
+    #   @param [Object] value the value to set
+    #   @return [Object] the new value
+
+    # @!macro fiber_local_var_method_bind
+    #
+    #   Bind the given value to fiber local storage during
+    #   execution of the given block.
+    #
+    #   @param [Object] value the value to bind
+    #   @yield the operation to be performed with the bound variable
+    #   @return [Object] the value
+
     def initialize(default = nil, &default_block)
       if default && block_given?
         raise ArgumentError, "Cannot use both value and block as default value"
@@ -15,20 +48,20 @@ module Concurrent
         @default = default
       end
 
-      @name = :"concurrent_variable_#{object_id}"
+      @index = LOCALS.next_index(self)
     end
 
-    # @!macro thread_local_var_method_get
+    # @!macro fiber_local_var_method_get
     def value
-      Thread.current.fetch(@name) {default}
+      LOCALS.fetch(@index) {default}
     end
 
-    # @!macro thread_local_var_method_set
+    # @!macro fiber_local_var_method_set
     def value=(value)
-      Thread.current[@name] = value
+      LOCALS.set(@index, value)
     end
 
-    # @!macro thread_local_var_method_bind
+    # @!macro fiber_local_var_method_bind
     def bind(value, &block)
       if block_given?
         old_value = self.value
