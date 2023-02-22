8453a46bdc4bdc9bca881143c0e81bcdae3644ff
Benoit Daloze <eregontp@gmail.com>
2023-02-22 19:07:58 +0100

    Revert "Prefer `Process.pid` over `$$`"
    This reverts commit 993a2529683a8aa0bf07fd81590365a5f4908d74.

--- lib/concurrent-ruby/concurrent/async.rb
+++ lib/concurrent-ruby/concurrent/async.rb
@@ -290,7 +290,7 @@ module Concurrent
         @delegate = delegate
         @queue = []
         @executor = Concurrent.global_io_executor
-        @ruby_pid = Process.pid
+        @ruby_pid = $$
       end
 
       # Delegates method calls to the wrapped object.
@@ -346,9 +346,9 @@ module Concurrent
       end
 
       def reset_if_forked
-        if Process.pid != @ruby_pid
+        if $$ != @ruby_pid
           @queue.clear
-          @ruby_pid = Process.pid
+          @ruby_pid = $$
         end
       end
     end
--- lib/concurrent-ruby/concurrent/executor/timer_set.rb
+++ lib/concurrent-ruby/concurrent/executor/timer_set.rb
@@ -76,7 +76,7 @@ module Concurrent
       @task_executor      = Options.executor_from_options(opts) || Concurrent.global_io_executor
       @timer_executor     = SingleThreadExecutor.new
       @condition          = Event.new
-      @ruby_pid           = Process.pid # detects if Ruby has forked
+      @ruby_pid           = $$ # detects if Ruby has forked
     end
 
     # Post the task to the internal queue.
@@ -127,10 +127,10 @@ module Concurrent
     end
 
     def ns_reset_if_forked
-      if Process.pid != @ruby_pid
+      if $$ != @ruby_pid
         @queue.clear
         @condition.reset
-        @ruby_pid = Process.pid
+        @ruby_pid = $$
       end
     end
 
