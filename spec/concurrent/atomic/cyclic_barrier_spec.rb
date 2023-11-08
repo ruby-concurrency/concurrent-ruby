a8fa48632c07e7cc163046f4669de649a3e8d05a
Charles Oliver Nutter <headius@headius.com>
2023-11-07 19:23:29 -0600

    Use repeat_until_success to wait for waiters
    This avoids getting stuck and waiting forever if something goes
    awry.

--- spec/concurrent/atomic/cyclic_barrier_spec.rb
+++ spec/concurrent/atomic/cyclic_barrier_spec.rb
@@ -71,7 +71,7 @@ module Concurrent
         end
 
         start_latch.wait(1)
-        Thread.pass until barrier.number_waiting == 1
+        repeat_until_success { expect(barrier.number_waiting).to eq 1 }
         barrier.reset
 
         expect(barrier).not_to be_broken
